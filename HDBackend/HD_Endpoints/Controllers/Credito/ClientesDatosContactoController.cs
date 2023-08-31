8af4fa91c770b400dcc8d75b5bd3251e3a494d8a
practicantehumaya <practicantesistemas@humaya.com.mx>
2023-08-31 15:47:56 -0700

    commit Contactos backend

--- HDBackend/HD_Endpoints/Controllers/Credito/ClientesDatosContactoController.cs
+++ HDBackend/HD_Endpoints/Controllers/Credito/ClientesDatosContactoController.cs
@@ -18,7 +18,7 @@ namespace HD.Endpoints.Controllers.Credito
         public async Task<ActionResult> Post(mdlClientes_Datos_Contacto mdl)
         {
 
-            string CadenaConexion = Configuracion["ConnectionStrings:Login"];
+            string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
             AD_ClientesDatosContacto_Guardar datos = new AD_ClientesDatosContacto_Guardar(CadenaConexion);
             mdl.usuario = Sesion.usuario();
             await datos.Guardar(mdl);
@@ -30,7 +30,7 @@ namespace HD.Endpoints.Controllers.Credito
         [Route("/api/[controller]/[action]")]
         public async Task<ActionResult> Listado(int idcliente)
         {
-            string CadenaConexion = Configuracion["ConnectionStrings:Login"];
+            string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
             AD_ClientesDatosContacto_Listado datos = new AD_ClientesDatosContacto_Listado(CadenaConexion);
             var result = await datos.Listado(idcliente);
             return Ok(result);
@@ -40,7 +40,7 @@ namespace HD.Endpoints.Controllers.Credito
         [Route("/api/[controller]/[action]/{id}")]
         public async Task<ActionResult> BuscarID(int idcliente, int orden)
         {
-            string CadenaConexion = Configuracion["ConnectionStrings:Login"];
+            string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
             AD_ClientesDatosContacto_BuscarID datos = new AD_ClientesDatosContacto_BuscarID(CadenaConexion);
             var result = await datos.BuscarID(idcliente, orden);
             return Ok(result);
