98f2dd19efbbbe533f7d53d412e1bcf9ced622b5
practicantehumaya <practicantesistemas@humaya.com.mx>
2023-09-01 16:14:13 -0700

    Backend Cultivos
    modelos y controladores para pantalla cultivos

---
+++ HDBackend/HD_Clientes/Consultas/ClientesCultivo/AD_ClientesCultivo_Guardar.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using HD.AccesoDatos;
+using HD.Clientes.Modelos;
+
+namespace HD.Clientes.Consultas.ClientesCultivo
+{
+    public class AD_ClientesCultivo_Guardar
+    {
+        private string CadenaConexion;
+        public AD_ClientesCultivo_Guardar(string _cadenaconexion)
+        {
+            CadenaConexion = _cadenaconexion;
+        }
+        public async Task<IEnumerable<mdlClientes_Cultivo>> Guardar(mdlClientes_Cultivo mdl)
+        {
+            try
+            {
+                FactoryConection factory = new FactoryConection(CadenaConexion);
+                var parametros = new
+                {
+                    idcliente = mdl.idcliente,
+                    registro = mdl.registro,
+                    idcultivo= mdl.idcultivo,
+                    terreno = mdl.terreno,
+                    hectareas = mdl.hectareas,
+                    seguro_cosecha = mdl.seguro_cosecha,
+                    ciclo = mdl.ciclo,
+                    tipo_riego = mdl.tipo_riego,
+                    temporal = mdl.temporal,
+                    rendimiento = mdl.rendimiento,
+                    precio = mdl.precio,
+                    mescosecha = mdl.mescosecha,
+                    estatus = mdl.estatus,
+                    usuario = mdl.usuario
+                };
+                IEnumerable<mdlClientes_Cultivo> result = await factory.SQL.QueryAsync<mdlClientes_Cultivo>("Credito.sp_Clientes_Cultivo_Guardar", parametros, commandType: System.Data.CommandType.StoredProcedure);
+                factory.SQL.Close();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Excepciones(System.Net.HttpStatusCode.InternalServerError, new { Mensaje = ex.Message });
+            }
+        }
+    }
+}
