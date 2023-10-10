ca911c9b79ef4efbe9ed898dbac0d50ddf72e98f
gpeol91 <93731144+gpeol91@users.noreply.github.com>
2023-10-09 20:22:07 -0700

    Reporte de cobranza

---
+++ HDBackend/HD_Endpoints/Controllers/Cobranza/TotalCarteraPorSucursalController.cs
@@ -0,0 +1,28 @@
+﻿using HD.Clientes.Consultas.ClientesDomicilio;
+using HD.Security;
+using HD_Cobranza.Capturas;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HD.Endpoints.Controllers.Cobranza
+{
+    public class TotalCarteraPorSucursalController:MyBase
+    {
+        private readonly IConfiguration Configuracion;
+        private readonly ISesion Sesion;
+        public TotalCarteraPorSucursalController(IConfiguration configuration, ISesion sesion)
+        {
+            Configuracion = configuration;
+            Sesion = sesion;
+        }
+        [HttpGet]
+        [Route("/api/[controller]/[action]")]
+        public async Task<ActionResult> Listado()
+        {
+            string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
+            ADCob_TotalCarteraPorSucursal datos = new ADCob_TotalCarteraPorSucursal(CadenaConexion);
+            var result = await datos.Listado();
+            return Ok(result);
+
+        }
+    }
+}
