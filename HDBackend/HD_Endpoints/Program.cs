83d6780af81cf82c98861cb34f31e926e79e2b17
Desarrolladorti2 <145804129+Desarrolladorti2@users.noreply.github.com>
2023-10-11 12:28:19 -0700

    -Solicitud de credito: *Buscar *Guardar

--- HDBackend/HD_Endpoints/Program.cs
+++ HDBackend/HD_Endpoints/Program.cs
@@ -10,7 +10,6 @@ Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
