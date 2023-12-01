586bc80b42d733704c213a479e922603aca2cf93
gpeol91 <93731144+gpeol91@users.noreply.github.com>
2023-12-01 13:10:44 -0700

    Impresion de Pedido

--- HDBackend/HD_Clientes/Consultas/SolicitudCredito/AD_SolicitudCredito_Detalle.cs
+++ HDBackend/HD_Clientes/Consultas/SolicitudCredito/AD_SolicitudCredito_Detalle.cs
@@ -11,14 +11,15 @@ namespace HD.Clientes.Consultas.SolicitudCredito
         {
             CadenaConexion = _cadenaconexion;
         }
-        public async Task<mdlView_Solicitud_Credito> Detalle(string folio)
+        public async Task<mdlView_Solicitud_Credito> Detalle(string folio,string usuario)
         {
             try
             {
                 FactoryConection factory = new FactoryConection(CadenaConexion);
                 var parametros = new
                 {
-                    folio
+                    folio,
+                    usuario
                 };
                 var result = await factory.SQL.QueryMultipleAsync("Credito.sp_Obtener_SolicitudCredito_Detalle", parametros, commandType: System.Data.CommandType.StoredProcedure);
                 mdlSolicitud_Credito_Detalle? detalle = result.Read<mdlSolicitud_Credito_Detalle>().FirstOrDefault();
---
+++ HDBackend/HD_Endpoints/Controllers/Credito/PedidoImpresionController.cs
@@ -0,0 +1,27 @@
+﻿using HD.Clientes.Consultas.PedidoImpresion;
+using HD.Security;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HD.Endpoints.Controllers.Credito
+{
+    public class PedidoImpresionController : MyBase
+    {
+        private readonly IConfiguration Configuracion;
+        private readonly ISesion Sesion;
+        public PedidoImpresionController(IConfiguration configuration, ISesion sesion)
+        {
+            Configuracion = configuration;
+            Sesion = sesion;
+        }
+        [HttpGet]
+        [Route("/api/[controller]/[action]")]
+        public async Task<ActionResult> PDF(string folio)
+        {
+            string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
+            ADPedido_Impresion_View datos = new ADPedido_Impresion_View(CadenaConexion);
+            var result = await datos.Get(folio);
+            return Ok(result);
+
+        }
+    }
+}
