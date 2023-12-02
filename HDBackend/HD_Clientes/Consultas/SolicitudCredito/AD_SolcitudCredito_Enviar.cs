b215a7ed3a97a557f0da1a3d0932d17b305431f7
gpeol91 <93731144+gpeol91@users.noreply.github.com>
2023-12-02 13:03:02 -0700

    Asistente

---
+++ HDBackend/HD_Clientes/Consultas/SolicitudCredito/AD_SolcitudCredito_Enviar.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using HD.AccesoDatos;
+using HD.Clientes.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HD.Clientes.Consultas.SolicitudCredito
+{
+    public class AD_SolcitudCredito_Enviar
+    {
+        private string CadenaConexion;
+        public AD_SolcitudCredito_Enviar(string _cadenaconexion)
+        {
+            CadenaConexion = _cadenaconexion;
+        }
+        public async Task<mdlSolicitudCredito_Enviar> Detalle(string folio, string usuario)
+        {
+            try
+            {
+                FactoryConection factory = new FactoryConection(CadenaConexion);
+                var parametros = new
+                {
+                    folio,
+                    usuario
+                };
+                var result = await factory.SQL.QueryFirstOrDefaultAsync<mdlSolicitudCredito_Enviar>("Credito.sp_Solicitud_Credito_Enviar", parametros, commandType: System.Data.CommandType.StoredProcedure);
+                factory.SQL.Close();
+                return result;
+            }
+            catch (System.Exception ex)
+            {
+                throw new Excepciones(System.Net.HttpStatusCode.InternalServerError, new { Mensaje = ex.Message });
+            }
+        }
+    }
+}
--- HDBackend/HD_Clientes/Modelos/mdlClientes_Datos_Contacto.cs
+++ HDBackend/HD_Clientes/Modelos/mdlClientes_Datos_Contacto.cs
@@ -13,23 +13,23 @@ namespace HD.Clientes.Modelos
         public int orden { get; set; }
 
         [Required(ErrorMessage = "El Medio de Contacto es un valor requerido")]
-        [RegularExpression(@"^[TWC]+$", ErrorMessage = "El campo Medio de Contacto debe estar formado  por las siguientes opciones [T][W][C]")]
-        [StringLength(1, MinimumLength = 1, ErrorMessage = "El campo Medio de Contacto debe estar formado por 1 digitos")]
+        [RegularExpression(@"^[T|W|C]+$", ErrorMessage = "El campo medio de contacto debe estar formado  por las siguientes opciones [T][W][C]")]
+        [StringLength(1, MinimumLength = 1, ErrorMessage = "El campo medio de contacto debe estar formado por 1 digitos")]
         public string? medio_contacto { get; set; } = "";
 
         [Required(ErrorMessage = "El Tipo de Contacto es un valor requerido")]
-        [RegularExpression(@"^[COVE]+$", ErrorMessage = "El campo Tipo de Contacto debe estar formado  por las siguientes opciones [CO][VE]")]
-        [StringLength(2, MinimumLength = 2, ErrorMessage = "El campo Medio de Contacto debe estar formado por 2 digitos")]
+        [RegularExpression(@"^[COVE]+$", ErrorMessage = "El campo tipo de contacto debe estar formado  por las siguientes opciones [CO][VE]")]
+        [StringLength(2, MinimumLength = 2, ErrorMessage = "El campo tipo de contacto debe estar formado por 2 digitos")]
         public string? tipo_contacto { get; set; }="";
 
         [Required(ErrorMessage = "El Valor es un valor requerido")]
         [RegularExpression(@"^[a-zA-Z0-9@-_.]+$", ErrorMessage = "El campo Tipo de Contacto debe estar formado  por las siguientes opciones [CO][VE]")]
-        [StringLength(150, MinimumLength = 10, ErrorMessage = "El campo Medio de Contacto debe estar formado por 1 digitos")]
+        [StringLength(150, MinimumLength = 10, ErrorMessage = "El campo valor debe estar formado como minimo por 10 caracteres")]
         public string? valor { get; set; } = "";
 
   
-        [RegularExpression(@"^[a-zA-Z0-9 ]+$", ErrorMessage = "El campo Comentarios debe estar formado por valores alfanumericos")]
-        [StringLength(maximumLength:500, ErrorMessage = "El campo Comentarios debe de contener una longitud maxima de 500 caracteres")]
+        [RegularExpression(@"^[a-zA-Z0-9 ]+$", ErrorMessage = "El campo comentarios debe estar formado por valores alfanumericos")]
+        [StringLength(maximumLength:500, ErrorMessage = "El campo comentarios debe de contener una longitud maxima de 500 caracteres")]
         public string? comentarios { get; set; } = "";
 
         public bool estatus { get; set; }
