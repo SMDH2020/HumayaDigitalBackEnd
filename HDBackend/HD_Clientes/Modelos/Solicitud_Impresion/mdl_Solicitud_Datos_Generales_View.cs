4c08a08aaef576b1b5c2d7327f5d6f23cb169368
practicantehumaya <practicantesistemas@humaya.com.mx>
2023-12-04 11:50:02 -0700

    Creacion de vista de solicitud credito
    Creación de modelos y controlador de solicitud de crédito

---
+++ HDBackend/HD_Clientes/Modelos/Solicitud_Impresion/mdl_Solicitud_Datos_Generales_View.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HD.Clientes.Modelos.Solicitud_Impresion
+{
+    public class mdl_Solicitud_Datos_Generales_View
+    {
+        public string? folio { get; set; }
+        public string? rfc { get; set; }
+        public string? tipopersona { get; set; }
+        public string?  curp { get; set; }
+        public string? razonsocial { get; set; }
+        public string? sexo { get; set; }
+        public string? estadocivil { get; set; }
+        public string? regimenconyugal { get; set; }
+        public string? edad { get; set; }
+        public string? tiempoagricultor { get; set; }
+        public string? agrupacion { get; set; }
+    }
+}
