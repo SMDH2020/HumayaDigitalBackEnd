29217f4f389add90f5c854f3e4014a7286b00233
gpeol91 <93731144+gpeol91@users.noreply.github.com>
2024-01-09 18:37:29 -0700

    Registro de clientes

--- HDBackend/HD_Clientes/Consultas/Clientes/AD_Clientes_Guardar.cs
+++ HDBackend/HD_Clientes/Consultas/Clientes/AD_Clientes_Guardar.cs
@@ -12,7 +12,7 @@ namespace HD.Clientes.Consultas.Clientes
         {
             CadenaConexion = _cadenaconexion;
         }
-        public async Task<int> Guardar(mdlClientes_Datos_Persona_Fisica mdl)
+        public async Task<int> Guardar_Persona_Moral(mdlClientes mdl)
         {
             FactoryConection factory = new FactoryConection(CadenaConexion);
             try
@@ -33,6 +33,36 @@ namespace HD.Clientes.Consultas.Clientes
                     usuario = mdl.usuario
                 };
                 mdl.idcliente= await factory.SQL.QueryFirstAsync<int>("Credito.sp_clientes_Guardar", parametros, commandType: System.Data.CommandType.StoredProcedure);
+                factory.SQL.Close();
+                return mdl.idcliente;
+            }
+            catch (Exception ex)
+            {
+                factory.SQL.Close();
+                throw new Excepciones(System.Net.HttpStatusCode.InternalServerError, new { Mensaje = ex.Message });
+            }
+        }
+        public async Task<int> Guardar_Persona_Fisica(mdlClientes_Datos_Persona_Fisica mdl)
+        {
+            FactoryConection factory = new FactoryConection(CadenaConexion);
+            try
+            {
+
+                var parametros = new
+                {
+                    idcliente = mdl.idcliente,
+                    rfc = mdl.rfc,
+                    razon_social = mdl.razon_social,
+                    tipo_persona = mdl.tipo_persona,
+                    medio_contacto = mdl.medio_contacto,
+                    tiempo_agricultor = mdl.tiempo_agricultor,
+                    agrupacion = mdl.agrupacion,
+                    regimen_fiscal = mdl.regimen_fiscal,
+                    tipo_venta = mdl.tipo_venta,
+                    estatus = mdl.estatus,
+                    usuario = mdl.usuario
+                };
+                mdl.idcliente = await factory.SQL.QueryFirstAsync<int>("Credito.sp_clientes_Guardar", parametros, commandType: System.Data.CommandType.StoredProcedure);
                 if (mdl.idcliente > 0)
                 {
                     factory.SQL.Close();
--- HDBackend/HD_Endpoints/Controllers/Credito/ClientesController.cs
+++ HDBackend/HD_Endpoints/Controllers/Credito/ClientesController.cs
@@ -14,15 +14,26 @@ namespace HD.Endpoints.Controllers.Credito
             Configuracion = configuration;
             Sesion = sesion;
         }
+        
         [HttpPost]
-        public async Task<ActionResult> Post(mdlClientes_Datos_Persona_Fisica mdl)
+        [Route("/api/[controller]/[action]")]
+        public async Task<ActionResult> Persona_Fisica(mdlClientes_Datos_Persona_Fisica mdl)
         {
             string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
             AD_Clientes_Guardar datos = new AD_Clientes_Guardar(CadenaConexion);
             mdl.usuario = Sesion.usuario();
-            mdl.idcliente = await datos.Guardar(mdl);
-            //AD_ClientesDatosPersonaFisica_Guardar datosfisica = new AD_ClientesDatosPersonaFisica_Guardar(CadenaConexion);
-            //await datosfisica.Guardar(mdl);
+            mdl.idcliente = await datos.Guardar_Persona_Fisica(mdl);
+            return Ok(new { mensaje = "datos cargados con exito" });
+        }
+
+        [HttpPost]
+        [Route("/api/[controller]/[action]")]
+        public async Task<ActionResult> Persona_Moral(mdlClientes mdl)
+        {
+            string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
+            AD_Clientes_Guardar datos = new AD_Clientes_Guardar(CadenaConexion);
+            mdl.usuario = Sesion.usuario();
+            mdl.idcliente = await datos.Guardar_Persona_Moral(mdl);
             return Ok(new { mensaje = "datos cargados con exito" });
         }
 
