81092d1b62d8772d4e0b18244982859dc19cc334
practicantehumaya <practicantesistemas@humaya.com.mx>
2024-01-22 11:03:52 -0700

    MVC Screen intereses y documentos
    Backend para pantalla de intereses y documentos

---
+++ HDBackend/HD_Clientes/Consultas/Documentos/AD_Documentos_Guardar.cs
@@ -0,0 +1,45 @@
+﻿using Dapper;
+using HD.AccesoDatos;
+using HD.Clientes.Modelos;
+
+namespace HD.Clientes.Consultas.Documentos
+{
+    public class AD_Documentos_Guardar
+    {
+        private string CadenaConexion;
+        public AD_Documentos_Guardar(string _cadenaconexion)
+        {
+            CadenaConexion = _cadenaconexion;
+        }
+        public async Task<bool> Guardar(mdlDocumentos mdl)
+        {
+            FactoryConection factory = new FactoryConection(CadenaConexion);
+            try
+            {
+
+                var parametros = new
+                {
+                    iddocumento = mdl.iddocumento,
+                    documento = mdl.documento,
+                    tipopersona = mdl.tipopersona,
+                    linea_credito = mdl.linea_credito,
+                    Opcional = mdl.Opcional,
+                    Documentacion = mdl.Documentacion,
+                    fase2 = mdl.fase2,
+                    jdf = mdl.jdf,
+                    dias_vigencia = mdl.dias_vigencia,
+                    estatus = mdl.estatus,
+                    usuario = mdl.usuario
+                };
+                await factory.SQL.QueryAsync("Credito.sp_Documentos_Guardar", parametros, commandType: System.Data.CommandType.StoredProcedure);
+                factory.SQL.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                factory.SQL.Close();
+                throw new Excepciones(System.Net.HttpStatusCode.InternalServerError, new { Mensaje = ex.Message });
+            }
+        }
+    }
+}
---
+++ HDBackend/HD_Clientes/Consultas/InteresMensual/AD_InteresMensual_Guardar.cs
@@ -0,0 +1,49 @@
+﻿using Dapper;
+using HD.AccesoDatos;
+using HD.Clientes.Consultas.ClientesDatosPersonaFisica;
+using HD.Clientes.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HD.Clientes.Consultas.InteresMensual
+{
+    public class AD_InteresMensual_Guardar
+    {
+        private string CadenaConexion;
+        public AD_InteresMensual_Guardar(string _cadenaconexion)
+        {
+            CadenaConexion = _cadenaconexion;
+        }
+        public async Task<bool> Guardar(mdlInteres_Mensual mdl)
+        {
+            FactoryConection factory = new FactoryConection(CadenaConexion);
+            try
+            {
+
+                var parametros = new
+                {
+                    idinteres = mdl.idinteres,
+                    ejercicio = mdl.ejercicio,
+                    periodo = mdl.periodo,
+                    interes = mdl.interes,
+                    documento = mdl.documento,
+                    extension = mdl.extension,
+                    estatus = mdl.estatus,
+                    usuario = mdl.usuario
+                };
+                await factory.SQL.QueryAsync("Credito.sp_Interes_Mensual_Guardar", parametros, commandType: System.Data.CommandType.StoredProcedure);
+                factory.SQL.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                factory.SQL.Close();
+                throw new Excepciones(System.Net.HttpStatusCode.InternalServerError, new { Mensaje = ex.Message });
+            }
+        }
+      
+    }
+}
