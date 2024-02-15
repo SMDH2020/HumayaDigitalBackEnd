e2c6c630a374b3accb5a98c70a623406471950f5
gpeol91 <93731144+gpeol91@users.noreply.github.com>
2024-02-15 11:24:23 -0700

    convenio pago

---
+++ HDBackend/HD_Cobranza/Capturas/ConvenioPago/ADCPClientesPredictivo.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using HD.AccesoDatos;
+using HD_Cobranza.Modelos.ConvenioPago;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HD_Cobranza.Capturas.ConvenioPago
+{
+    public class ADCPClientesPredictivo
+    {
+        private string CadenaConexion;
+        public ADCPClientesPredictivo(string _cadenaconexion)
+        {
+            CadenaConexion = _cadenaconexion;
+        }
+        public async Task<IEnumerable<mdlCPClientesPredictivo>> Listado(string idcliente)
+        {
+            try
+            {
+                FactoryConection factory = new FactoryConection(CadenaConexion);
+                var parametros = new
+                {
+                    idcliente
+                };
+
+                IEnumerable<mdlCPClientesPredictivo> result = await factory.SQL.QueryAsync<mdlCPClientesPredictivo>("Cobranza.SP_Obtener_Clientes_Registrados", parametros, commandType: System.Data.CommandType.StoredProcedure);
+                factory.SQL.Close();
+                return result;
+            }
+            catch (System.Exception ex)
+            {
+                throw new Excepciones(System.Net.HttpStatusCode.InternalServerError, new { Mensaje = ex.Message });
+            }
+        }
+    }
+}
