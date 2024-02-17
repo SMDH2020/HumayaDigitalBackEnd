83ee5395fbe92b7846658826bf0d94db728618ef
gpeol91 <93731144+gpeol91@users.noreply.github.com>
2024-02-17 11:48:50 -0700

    actualizacion sabado 17 febrero 2024

--- HDBackend/HD_Cobranza/Capturas/ConvenioPago/ADVencimientosSaldos.cs
+++ HDBackend/HD_Cobranza/Capturas/ConvenioPago/ADVencimientosSaldos.cs
@@ -35,6 +35,26 @@ namespace HD_Cobranza.Capturas.ConvenioPago
                 throw new Excepciones(System.Net.HttpStatusCode.InternalServerError, new { Mensaje = ex.Message });
             }
         }
+
+        public async Task<IEnumerable<mdlVencidosOperacion>> ObtenerRevolventeob(int idcliente)
+        {
+            try
+            {
+                FactoryConection factory = new FactoryConection(CadenaConexion);
+                var parametros = new
+                {
+                    idcliente
+                };
+
+                IEnumerable<mdlVencidosOperacion> result = await factory.SQL.QueryAsync<mdlVencidosOperacion>("Cobranza.Saldo_Vencido_Por_Cliente_Revolvente", parametros, commandType: System.Data.CommandType.StoredProcedure);
+                factory.SQL.Close();
+                return result;
+            }
+            catch (System.Exception ex)
+            {
+                throw new Excepciones(System.Net.HttpStatusCode.InternalServerError, new { Mensaje = ex.Message });
+            }
+        }
         public async Task<IEnumerable<mdlVencidosOperacion>> ObtenerOperacion(int idcliente)
         {
             try
