642f2f6bfc9e1cc2891293ebd751367065af97a9
practicantehumaya <practicantesistemas@humaya.com.mx>
2024-03-01 09:11:39 -0700

    Menus Screen

--- HDBackend/HD_Generales/Autenticate/mdlMenu.cs
+++ HDBackend/HD_Generales/Autenticate/mdlMenu.cs
@@ -3,7 +3,11 @@
     public class mdlMenu
     {
         public int idmenu { get; set; }
+        public int idmodulo { get; set; }
+        public string modulo { get; set; } = "";
         public string descripcion { get; set; } = "";
         public string nomenclatura { get; set; } = "";
+        public bool estatus { get; set; }
+        public string? usuario { get; set; } = "";
     }
 }
