ffab8b70d5675a3f96e28c677dbbf32990b18608
gpeol91 <93731144+gpeol91@users.noreply.github.com>
2024-03-03 22:51:39 -0700

    facturacion cajera

--- HDBackend/HD_Endpoints/Controllers/Clientes/FacturarEquipoController.cs
+++ HDBackend/HD_Endpoints/Controllers/Clientes/FacturarEquipoController.cs
@@ -26,5 +26,21 @@ namespace HD.Endpoints.Controllers.Clientes
             return Ok(result);
 
         }
+
+        [HttpGet]
+        [Route("/api/[controller]/[action]")]
+        public async Task<ActionResult> informacion_pedido(string folio)
+        {
+            if(folio ==null || folio .Length != 13)
+            {
+                return BadRequest("la información proporcionada no es correcta");
+            }
+            string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
+            AD_Facturar_Equipo_Estado datos = new AD_Facturar_Equipo_Estado(CadenaConexion);
+            int usuario = int.Parse(Sesion.usuario());
+            var result = await datos.informacion(folio, usuario);
+            return Ok(result);
+
+        }
     }
 }
