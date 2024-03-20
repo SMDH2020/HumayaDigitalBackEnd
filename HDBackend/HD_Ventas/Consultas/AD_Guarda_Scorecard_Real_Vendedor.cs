a7f910b1e35d1ecfa6dc8ab19e3d64d11f6bb959
Desarrolladorti2 <145804129+Desarrolladorti2@users.noreply.github.com>
2024-03-20 09:30:08 -0700

    Back scorecard objetivo y real

---
+++ HDBackend/HD_Ventas/Consultas/AD_Guarda_Scorecard_Real_Vendedor.cs
@@ -0,0 +1,44 @@
+﻿using Dapper;
+using HD.AccesoDatos;
+using HD_Ventas.Modelos;
+
+namespace HD_Ventas.Consultas
+{
+    public class AD_Guarda_Scorecard_Real_Vendedor
+    {
+        private string CadenaConexion;
+        public AD_Guarda_Scorecard_Real_Vendedor(string _cadenaconexion)
+        {
+            CadenaConexion = _cadenaconexion;
+        }
+        public async Task<IEnumerable<mdlGuarda_Scorecard_Real_Vendedor>>
+
+            Guardar(mdlGuarda_Scorecard_Real_Vendedor mdl)
+        {
+            try
+            {
+                FactoryConection factory = new FactoryConection(CadenaConexion);
+                var parametros = new
+                {
+                    @ejercicio = mdl.ejercicio,
+                    @sucursal = mdl.sucursal,
+                    @idvendedor = mdl.idvendedor,
+                    @usuario = mdl.usuario,
+                    @scorecard_detalle = mdl.scorecard_detalle,
+                };
+
+                var result = await
+                factory.SQL.QueryAsync<mdlGuarda_Scorecard_Real_Vendedor>("Ventas.sp_Scorecard_Real_Vendedor_Guardar",
+                parametros, commandType: System.Data.CommandType.StoredProcedure);
+                factory.SQL.Close();
+                return result;
+            }
+            catch (System.Exception ex)
+            {
+                throw new
+                Excepciones(System.Net.HttpStatusCode.InternalServerError, new { Mensaje = ex.Message });
+            }
+
+        }
+    }
+}
---
+++ HDBackend/HD_Ventas/Modelos/mdlListado_Vendedores.cs
@@ -0,0 +1,8 @@
+﻿namespace HD_Ventas.Modelos
+{
+    public class mdlListado_Vendedores
+    {
+        public int idempleado {  get; set; }
+        public string? empleado { get; set; }
+    }
+}
