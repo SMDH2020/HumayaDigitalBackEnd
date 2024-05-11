711253d45fbbb1964fd7734ab5cbfa5551422fc5
gpeol91 <93731144+gpeol91@users.noreply.github.com>
2024-05-11 11:55:28 -0700

    sabado 11052024

---
+++ HDBackend/HD_Clientes/Consultas/Facturar_Equipo/AD_FacturarEquipo_CreditoOtorgado.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using HD.AccesoDatos;
+using HD.Clientes.Modelos.Facturar_Equipo;
+
+namespace HD.Clientes.Consultas.Facturar_Equipo
+{
+    public class AD_FacturarEquipo_CreditoOtorgado
+    {
+        private string CadenaConexion;
+        public AD_FacturarEquipo_CreditoOtorgado(string _cadenaconexion)
+        {
+            CadenaConexion = _cadenaconexion;
+        }
+        public async Task<IEnumerable<mdl_Factuar_Equipo_View>> Listado(int usuario)
+        {
+            try
+            {
+                var parametros = new
+                {
+                    usuario
+                };
+                FactoryConection factory = new FactoryConection(CadenaConexion);
+                IEnumerable<mdl_Factuar_Equipo_View> result = await factory.SQL.QueryAsync<mdl_Factuar_Equipo_View>("Credito.sp_Solicitudes_por_Facturar", parametros, commandType: System.Data.CommandType.StoredProcedure);
+                factory.SQL.Close();
+                return result;
+            }
+            catch (System.Exception ex)
+            {
+                throw new Excepciones(System.Net.HttpStatusCode.InternalServerError, new { Mensaje = ex.Message });
+            }
+        }
+    }
+}
---
+++ HDBackend/HD_Endpoints/Controllers/Clientes/FacturarEquipoCreditoOtorgadoController.cs
@@ -0,0 +1,29 @@
+﻿using HD.Clientes.Consultas.Facturar_Equipo;
+using HD.Security;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HD.Endpoints.Controllers.Clientes
+{
+    public class FacturarEquipoCreditoOtorgadoController :MyBase
+    {
+        private readonly IConfiguration Configuracion;
+        private readonly ISesion Sesion;
+        public FacturarEquipoCreditoOtorgadoController(IConfiguration configuration, ISesion sesion)
+        {
+            Configuracion = configuration;
+            Sesion = sesion;
+        }
+
+        [HttpGet]
+        [Route("/api/[controller]/[action]")]
+        public async Task<ActionResult> Obtener_Solicitudes()
+        {
+            string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
+            AD_Facturar_Equipo datos = new AD_Facturar_Equipo(CadenaConexion);
+            int usuario = int.Parse(Sesion.usuario());
+            var result = await datos.Listado(usuario);
+            return Ok(result);
+
+        }
+    }
+}
