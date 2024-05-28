0452be279d6844e9d702b813a57259fc68820003
Desarrolladorti2 <145804129+Desarrolladorti2@users.noreply.github.com>
2024-05-28 11:24:25 -0700

    reporte buro back

---
+++ HDBackend/HD_Endpoints/Controllers/BuroCredito/CargaReporteClientesNoReportadosController.cs
@@ -0,0 +1,33 @@
+﻿using HD_Buro.Consultas;
+using HD.Security;
+using Microsoft.AspNetCore.Mvc;
+using HD_Buro.Modelos;
+using HD_Ventas.Consultas;
+using HD_Ventas.Modelos;
+
+namespace HD.Endpoints.Controllers.BuroCredito
+{
+    public class CargaReporteClientesNoReportadosController:MyBase
+    {
+        private readonly IConfiguration Configuracion;
+        private readonly ISesion Sesion;
+
+        public CargaReporteClientesNoReportadosController(IConfiguration configuration, ISesion sesion)
+        {
+            Configuracion = configuration;
+            Sesion = sesion;
+        }
+
+        [HttpGet]
+        [Route("/api/[controller]/[action]")]
+        public async Task<ActionResult> Cargar(int ejercicio, int periodo, int sucursal, string mostrar)
+        {
+            string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
+            AD_Carga_Clientes_NoReportados datos = new AD_Carga_Clientes_NoReportados(CadenaConexion);
+            int usuario = int.Parse(Sesion.usuario());
+            usuario = 8919;
+            var result = await datos.reporte(ejercicio, periodo, sucursal, mostrar);
+            return Ok(result);
+        }
+    }
+}
