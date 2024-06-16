e069ae85478ba6cd75998d2b095457367061bbc2
gpeol91 <93731144+gpeol91@users.noreply.github.com>
2024-06-16 13:44:28 -0700

    Reportes de recuperacion de cobranza

--- HDBackend/HD_Buro/Consultas/AD_Carga_Reporte_Buro.cs
+++ HDBackend/HD_Buro/Consultas/AD_Carga_Reporte_Buro.cs
@@ -11,7 +11,7 @@ namespace HD_Buro.Consultas
         {
             CadenaConexion = _cadenaconexion;
         }
-        public async Task<IEnumerable<mdlCarga_Reporte_Buro>> reporte(int ejercicio, int periodo, int sucursal, string lineacredito)
+        public async Task<IEnumerable<mdlCarga_Reporte_Buro>> reporte(int ejercicio, int periodo)
         {
             try
             {
@@ -19,11 +19,9 @@ namespace HD_Buro.Consultas
                 {
                     Ejercicio = ejercicio,
                     Periodo = periodo,
-                    Sucursal = sucursal,
-                    Lineacredito = lineacredito
                 };
                 FactoryConection factory = new FactoryConection(CadenaConexion);
-                IEnumerable<mdlCarga_Reporte_Buro> result = await factory.SQL.QueryAsync<mdlCarga_Reporte_Buro>("BuroCredito.dbo.sp_Clientes_Cierre_Mensual", parametros, commandType: System.Data.CommandType.StoredProcedure);
+                IEnumerable<mdlCarga_Reporte_Buro> result = await factory.SQL.QueryAsync<mdlCarga_Reporte_Buro>("Cartera_clientes.dbo.sp_obtener_Listado_Mensual_Credito_Mhusa", parametros, commandType: System.Data.CommandType.StoredProcedure);
                 factory.SQL.Close();
                 return result;
             }
---
+++ HDBackend/HD_Cobranza/Capturas/ADRecuperacionCarteraMensual.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using HD.AccesoDatos;
+using HD_Cobranza.Modelos;
+
+namespace HD_Cobranza.Capturas
+{
+    public class ADRecuperacionCarteraMensual
+    {
+        private string CadenaConexion;
+        public ADRecuperacionCarteraMensual(string _cadenaconexion)
+        {
+            CadenaConexion = _cadenaconexion;
+        }
+        public async Task<IEnumerable<mdlRecuperacionCarteraMenusal>> Obtener(int ejercicio)
+        {
+            try
+            {
+                FactoryConection factory = new FactoryConection(CadenaConexion);
+                var parametros = new
+                {
+                    ejercicio = ejercicio,
+                };
+
+                var result = await factory.SQL.QueryAsync<mdlRecuperacionCarteraMenusal>("Cartera_Clientes.dbo.sp_obtenere_Recuperacion_Cartera_Mensual", parametros, commandType: System.Data.CommandType.StoredProcedure);
+                factory.SQL.Close();
+                return result;
+            }
+            catch (System.Exception ex)
+            {
+                throw new Excepciones(System.Net.HttpStatusCode.InternalServerError, new { Mensaje = ex.Message });
+            }
+        }
+    }
+}
