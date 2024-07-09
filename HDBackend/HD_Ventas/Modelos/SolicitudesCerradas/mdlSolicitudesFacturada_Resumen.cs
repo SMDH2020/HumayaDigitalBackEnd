e7cfc7506d989e55d8d5c2b091963afa37ddd3b0
SistemasSinaloa <sistemassinaloa@humaya.com.mx>
2024-07-09 11:58:34 -0700

    actuializacion 09-07

--- HDBackend/HD_Ventas/Modelos/SolicitudesCerradas/mdlSolicitudesFacturada_Resumen.cs
+++ HDBackend/HD_Ventas/Modelos/SolicitudesCerradas/mdlSolicitudesFacturada_Resumen.cs
@@ -5,6 +5,7 @@
         public string titulo { get; set; }
         public int total_solicitudes { get; set; }
         public int cerradas { get; set; }
+        public int pendientes { get; set; }
         public int jdf { get; set; }
         public int mhusa { get; set; }
         public int contado { get; set; }
