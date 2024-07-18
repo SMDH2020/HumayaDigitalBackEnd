746005b5a11df3b739f93abd19f96d96e1e1dd82
SistemasSinaloa <sistemassinaloa@humaya.com.mx>
2024-07-18 16:58:46 -0700

    actualizacion enganche

--- HDBackend/HD_Endpoints/Controllers/AnalisisCredito/ACTimeLineComentariosController.cs
+++ HDBackend/HD_Endpoints/Controllers/AnalisisCredito/ACTimeLineComentariosController.cs
@@ -1,4 +1,5 @@
 ﻿using HD.Clientes.Consultas.AnalisisCredito;
+using HD.Clientes.Modelos;
 using HD.Clientes.Modelos.SC_Analisis;
 using HD.Notifications.Analisis;
 using HD.Security;
@@ -15,6 +16,7 @@ namespace HD.Endpoints.Controllers.AnalisisCredito
             Configuracion = configuration;
             Sesion = sesion;
         }
+
         [HttpPost]
         public async Task<ActionResult> Post(mdlSCAnalisis_Comentarios mdl)
         {
@@ -63,6 +65,29 @@ namespace HD.Endpoints.Controllers.AnalisisCredito
 
             await NotificacionComentarios.Enviar_Mhusa(result);
             return Ok(result.estado);
+        }
+
+        [HttpPost]
+        [Route("/api/[controller]/[action]")]
+        public async Task<ActionResult> GuardarEnganche(mdlEnchanche_Mhusa mdl)
+        {
+            string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
+            ADSolicitud_Credito_Enganche_Guardar datos = new ADSolicitud_Credito_Enganche_Guardar(CadenaConexion);
+            mdl.usuario = Sesion.usuario();
+            var result = await datos.Guardar(mdl);
+            if (result is null)
+            {
+                return BadRequest(new { mensaje = "Error al enviar correo, no se encontro información" });
+            }
+            else
+            {
+                if (result.mdldatos is null)
+                {
+                    return BadRequest(new { mensaje = "Error al enviar correo, no se encontro información" });
+                }
+                await NotificacionComentarios.Enviar_Mhusa(result);
+                return Ok(result.estado);
+            }
         }
     }
 }
