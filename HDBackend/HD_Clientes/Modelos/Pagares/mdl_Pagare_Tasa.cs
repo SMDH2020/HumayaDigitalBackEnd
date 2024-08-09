cb6a5938839e99fac893a62fb8a0b7c34a2f9d62
gpeol91 <93731144+gpeol91@users.noreply.github.com>
2024-08-09 10:53:06 -0700

    Actualizacion#

--- HDBackend/HD_Clientes/Modelos/Pagares/mdl_Pagare_Tasa.cs
+++ HDBackend/HD_Clientes/Modelos/Pagares/mdl_Pagare_Tasa.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HD.Clientes.Modelos.Pagares
+﻿namespace HD.Clientes.Modelos.Pagares
 {
     public class mdl_Pagare_Tasa
     {
         public double tasa { get; set; } = 0;
-        
+        public string? moneda { get; set; }
+
     }
 }
