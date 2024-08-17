c09352e3e343a52fc05b47e2b4843f62b7b62ba3
Desarrolladorti2 <desarrolladorti2@humaya.com.mx>
2024-08-16 23:45:22 -0700

    dashboard-finanzas-back

---
+++ HDBackend/HD_Finanzas/Modelos/RazonesFinancieras/mdlGastos.cs
@@ -0,0 +1,9 @@
+﻿
+namespace HD_Finanzas.Modelos.RazonesFinancieras
+{
+    public class mdlGastos
+    {
+        public int Periodo { get; set; }
+        public float gastos {  get; set; }
+    }
+}
