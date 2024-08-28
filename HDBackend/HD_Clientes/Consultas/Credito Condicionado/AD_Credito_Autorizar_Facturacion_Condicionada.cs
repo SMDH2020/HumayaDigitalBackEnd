251c7d24f12d7df8fe7bff53d89f4e99236cca15
gpeol91 <93731144+gpeol91@users.noreply.github.com>
2024-08-27 21:30:40 -0700

    ACtualizacion timeline condicionado#

---
+++ HDBackend/HD_Clientes/Consultas/Credito Condicionado/AD_Credito_Autorizar_Facturacion_Condicionada.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using HD.AccesoDatos;
+using HD.Clientes.Modelos.SC_Analisis.Credito_Condicionados;
+
+namespace HD.Clientes.Consultas.Credito_Condicionado
+{
+    public class AD_Credito_Autorizar_Facturacion_Condicionada
+    {
+        private string CadenaConexion;
+        FactoryConection factory;
+        public AD_Credito_Autorizar_Facturacion_Condicionada(string _cadenaconexion)
+        {
+            CadenaConexion = _cadenaconexion;
+            factory = new FactoryConection(CadenaConexion);
+        }
+        public async Task<mdl_Analisis_100_view> Guardar(mdl_Autorizar_facturacion_View mdl)
+        {
+            var parametros = new
+            {
+                folio = mdl.folio,
+                comentarios = mdl.comentarios,
+                fecha = mdl.fecha,
+                usuario = mdl.usuario,
+            };
+            var result = await factory.SQL.QueryMultipleAsync("Credito.sp_Aprobar_Facturacion_Condicionada", parametros, commandType: System.Data.CommandType.StoredProcedure);
+            mdl_Analisis_100_view view = new mdl_Analisis_100_view();
+            view.encabezado = result.Read<mdl_Analisis_100_encabezado>().FirstOrDefault();
+            view.detalle = result.Read<mdl_Analisis_100_detalle>().ToList();
+            return view;
+        }
+    }
+}
--- HDBackend/HD_Clientes/Consultas/SolicitudCreditoDocumento/ADSolicitud_Credito_Documentacion_Condicionada_Guardar.cs
+++ HDBackend/HD_Clientes/Consultas/SolicitudCreditoDocumento/ADSolicitud_Credito_Documentacion_Condicionada_Guardar.cs
@@ -1,11 +1,6 @@
 ﻿using Dapper;
 using HD.AccesoDatos;
 using HD.Clientes.Modelos;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HD.Clientes.Consultas.SolicitudCreditoDocumento
 {
