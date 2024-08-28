5570af28ff74ca8b065ec7eafb5da5ea89102900
SistemasSinaloa <sistemassinaloa@humaya.com.mx>
2024-08-28 12:19:41 -0700

    actualizacion

--- HDBackend/HD_Clientes/Consultas/AnalisisCredito/Modal/ADAnalisis_Documentacion.cs
+++ HDBackend/HD_Clientes/Consultas/AnalisisCredito/Modal/ADAnalisis_Documentacion.cs
@@ -62,6 +62,33 @@ namespace HD.Clientes.Consultas.AnalisisCredito.Modal
                 throw new Excepciones(System.Net.HttpStatusCode.InternalServerError, new { Mensaje = ex.Message });
             }
         }
+
+        public async Task<mdlSCAnalisis_Documentacion_View> GetOtorgamientoCondicionado(string folio, string usuario)
+        {
+            try
+            {
+                FactoryConection factory = new FactoryConection(CadenaConexion);
+                var parametros = new
+                {
+                    folio,
+                    usuario
+                };
+                var result = await factory.SQL.QueryMultipleAsync("Credito.sp_Analisis_Otorgamiento_Condicionado", parametros, commandType: System.Data.CommandType.StoredProcedure);
+                mdlSCAnalisis_Documentacion_View view = new mdlSCAnalisis_Documentacion_View();
+                view.estado = result.Read<mdlSCAnalisis_Pedido_Estado>().FirstOrDefault();
+                view.documentacion = result.Read<mdlSCAnalisis_Documentacion>().ToList();
+
+                if (view.estado is null) view.estado = new mdlSCAnalisis_Pedido_Estado();
+
+                factory.SQL.Close();
+                return view;
+            }
+            catch (System.Exception ex)
+            {
+                throw new Excepciones(System.Net.HttpStatusCode.InternalServerError, new { Mensaje = ex.Message });
+            }
+        }
+
         public async Task<IEnumerable<mdlSCAnalisis_Documentacion>> SetOtorgamientoMhusaDocumento(string folio,int iddocumento, string usuario)
         {
             try
@@ -185,6 +212,33 @@ namespace HD.Clientes.Consultas.AnalisisCredito.Modal
                 throw new Excepciones(System.Net.HttpStatusCode.InternalServerError, new { Mensaje = ex.Message });
             }
         }
+
+        public async Task<mdlSCAnalisis_Documentacion_View> AnalizarDocumentosAceptadosCondicionado(string folio, string usuario)
+        {
+            try
+            {
+                FactoryConection factory = new FactoryConection(CadenaConexion);
+                var parametros = new
+                {
+                    folio,
+                    usuario
+                };
+                var result = await factory.SQL.QueryMultipleAsync("Credito.sp_Analizar_Documentacion_Aceptada_Condicionado", parametros, commandType: System.Data.CommandType.StoredProcedure);
+                mdlSCAnalisis_Documentacion_View view = new mdlSCAnalisis_Documentacion_View();
+                view.estado = result.Read<mdlSCAnalisis_Pedido_Estado>().FirstOrDefault();
+                view.detalle = result.Read<mdl_Analisis_100_detalle>().ToList();
+
+                if (view.estado is null) view.estado = new mdlSCAnalisis_Pedido_Estado();
+
+                factory.SQL.Close();
+                return view;
+            }
+            catch (System.Exception ex)
+            {
+                throw new Excepciones(System.Net.HttpStatusCode.InternalServerError, new { Mensaje = ex.Message });
+            }
+        }
+
 
         public async Task<IEnumerable<mdlSCAnalisis_Documentacion>> SetOtorgamientoCondicionadoDocumento(string folio, int iddocumento, string usuario)
         {
