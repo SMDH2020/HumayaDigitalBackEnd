29ee71178e0719557876513c3ce4c33a7e75a0fd
Desarrolladorti2 <desarrolladorti2@humaya.com.mx>
2024-08-28 09:55:27 -0700

    Actualizacion-Back

--- HDBackend/HD_Dashboard/Modelos/mdlListado_Vendedores_Dash.cs
+++ HDBackend/HD_Dashboard/Modelos/mdlListado_Vendedores_Dash.cs
@@ -5,5 +5,7 @@
         public int idempleado { get; set; }
         public string? empleado { get; set; }
         public int IDSucursal { get; set; }
+        public string? Descripcion { get; set; }
+        public int IDEstado { get; set; }
     }
 }
