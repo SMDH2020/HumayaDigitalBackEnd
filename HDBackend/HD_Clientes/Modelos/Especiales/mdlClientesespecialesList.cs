6abc3b1f76d1f7428489be9c93d7625eadb02568
gpeol91 <93731144+gpeol91@users.noreply.github.com>
2024-11-13 15:20:11 -0700

    Actualizacion

---
+++ HDBackend/HD_Clientes/Modelos/Especiales/mdlClientesespecialesList.cs
@@ -0,0 +1,12 @@
+﻿namespace HD.Clientes.Modelos.Especiales
+{
+    public class mdlClientesespecialesList
+    {
+        public int idcliente { get; set; }
+        public string? razon_social { get; set; }
+        public string? comentarios { get; set; }
+        public string? estatus { get; set; }
+        public string? tipo_cliente { get; set; }
+        public string? empleado { get; set; }
+    }
+}
---
+++ HDBackend/HD_Clientes/Modelos/Especiales/mdlFacturasnocontemplar_List.cs
@@ -0,0 +1,10 @@
+﻿namespace HD.Clientes.Modelos.Especiales
+{
+    public class mdlFacturasnocontemplar_List
+    {
+        public string? documento { get; set; }
+        public string? comentarios { get; set; }
+        public string? estatus { get; set; }
+        public string? empleado { get; set; }
+    }
+}
