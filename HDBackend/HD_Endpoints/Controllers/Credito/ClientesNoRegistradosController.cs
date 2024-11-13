fc4784d394a67594d06b03bd662d9af1fe7c7215
Desarrolladorti2 <desarrolladorti2@humaya.com.mx>
2024-11-13 11:26:00 -0700

    Actualizacion Back

---
+++ HDBackend/HD_Endpoints/Controllers/Credito/ClientesNoRegistradosController.cs
@@ -0,0 +1,44 @@
+﻿using HD.Clientes.Consultas.Clientes;
+using HD.Clientes.Consultas.ClientesNoRegistrados;
+using HD.Clientes.Modelos;
+using HD.Security;
+using HD_Cobranza.Capturas.ConvenioPago;
+using HD_Cobranza.GestionCobranza.Capturas;
+using HD_Cobranza.Reportes;
+using HD_Reporteria.Credito;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HD.Endpoints.Controllers.Credito
+{
+    public class ClientesNoRegistradosController : MyBase
+    {
+        private readonly IConfiguration Configuracion;
+        private readonly ISesion Sesion;
+        public ClientesNoRegistradosController(IConfiguration configuration, ISesion sesion)
+        {
+            Configuracion = configuration;
+            Sesion = sesion;
+        }
+
+        [HttpGet]
+        [Route("/api/[controller]/[action]")]
+        public async Task<ActionResult> Listado(string ADR, string sucursales)
+        {
+            string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
+            AD_Clientes_No_Registrados datos = new AD_Clientes_No_Registrados(CadenaConexion);
+            var result = await datos.Clientes(ADR, sucursales);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("/api/[controller]/[action]")]
+        public async Task<ActionResult> ImprimirExcel(string adr, string sucursal)
+        {
+            string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
+            AD_Clientes_No_Registrados datos = new AD_Clientes_No_Registrados(CadenaConexion);
+            var result = await datos.Clientes(adr, sucursal);
+            var docresult = await XLSCre_Listado_Clientes_NoRegistrados.GenerarExcel(result);
+            return Ok(docresult);
+        }
+    }
+}
