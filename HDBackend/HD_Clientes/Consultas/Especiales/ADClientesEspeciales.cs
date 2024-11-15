71239ec62cbd886c7483a245c6ccd3b28b07e882
Desarrolladorti2 <desarrolladorti2@humaya.com.mx>
2024-11-15 13:07:25 -0700

    Actualizacion back

--- HDBackend/HD_Clientes/Consultas/Especiales/ADClientesEspeciales.cs
+++ HDBackend/HD_Clientes/Consultas/Especiales/ADClientesEspeciales.cs
@@ -17,11 +17,11 @@ namespace HD.Clientes.Consultas.Especiales
             {
                 var parametros = new
                 {
-                    idcliente = mdl.idcliente,
-                    comentarios = mdl.comentarios,
-                    estatus = mdl.estatus,
-                    tipo_cliente = mdl.tipo_cliente,
-                    usuario = mdl.usuario,
+                    @idcliente = mdl.idcliente,
+                    @comentarios = mdl.comentarios,
+                    @estatus = mdl.estatus,
+                    @tipo_cliente = mdl.tipo_cliente,
+                    @usuario = mdl.usuario,
                 };
                 FactoryConection factory = new FactoryConection(CadenaConexion);
                 var result = await factory.SQL.QueryMultipleAsync("Credito.sp_Clientes_Especiales_Guardar", parametros, commandType: System.Data.CommandType.StoredProcedure);
@@ -40,6 +40,61 @@ namespace HD.Clientes.Consultas.Especiales
             {
                 FactoryConection factory = new FactoryConection(CadenaConexion);
                 var result = await factory.SQL.QueryAsync<mdlClientesespecialesList>("Credito.sp_Clientes_Especiales_Listado", commandType: System.Data.CommandType.StoredProcedure);
+                factory.SQL.Close();
+                return result;
+            }
+            catch (System.Exception ex)
+            {
+                throw new Excepciones(System.Net.HttpStatusCode.InternalServerError, new { mensaje = ex.Message });
+            }
+        }
+
+        public async Task<IEnumerable<mdldropdownlistClientesEspeciales>> DropDownList()
+        {
+            try
+            {
+                FactoryConection factory = new FactoryConection(CadenaConexion);
+                var parametros = new
+                {
+                };
+                var result = await factory.SQL.QueryAsync<mdldropdownlistClientesEspeciales>("Credito.sp_clientes_especiales_dropdownlist", commandType: System.Data.CommandType.StoredProcedure);
+                factory.SQL.Close();
+                return result;
+            }
+            catch (System.Exception ex)
+            {
+                throw new Excepciones(System.Net.HttpStatusCode.InternalServerError, new { Mensaje = ex.Message });
+            }
+        }
+
+        public async Task<IEnumerable<mdldropdownlistClientesEspeciales>> DropDownListTodos()
+        {
+            try
+            {
+                FactoryConection factory = new FactoryConection(CadenaConexion);
+                var parametros = new
+                {
+                };
+                var result = await factory.SQL.QueryAsync<mdldropdownlistClientesEspeciales>("Credito.sp_clientes_especiales_dropdownlist_todos", commandType: System.Data.CommandType.StoredProcedure);
+                factory.SQL.Close();
+                return result;
+            }
+            catch (System.Exception ex)
+            {
+                throw new Excepciones(System.Net.HttpStatusCode.InternalServerError, new { Mensaje = ex.Message });
+            }
+        }
+
+        public async Task<IEnumerable<mdlClientesespecialesList>> InfoCliente(int idCliente)
+        {
+            try
+            {
+                FactoryConection factory = new FactoryConection(CadenaConexion);
+                var parametros = new
+                {
+                    @idCliente = idCliente
+                };
+                var result = await factory.SQL.QueryAsync<mdlClientesespecialesList>("Credito.sp_Obtener_Cliente_Especial_ID", parametros, commandType: System.Data.CommandType.StoredProcedure);
                 factory.SQL.Close();
                 return result;
             }
