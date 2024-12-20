2be77c1c56865f229d6a47c7f57309905c6f81f3
Desarrolladorti2 <desarrolladorti2@humaya.com.mx>
2024-12-20 10:52:58 -0700

    Actualizacion Back

---
+++ HDBackend/HD_Cobranza/Modelos/Dashboard/mdl_Dashboard_Objetivo_Total_View.cs
@@ -0,0 +1,11 @@
+﻿using HD_Cobranza.Modelos.NewFolder;
+
+namespace HD_Cobranza.Modelos.Dashboard
+{
+    public class mdl_Dashboard_Objetivo_Total_View
+    {
+        public IEnumerable<mdl_Dashboard_Objetivo_Total> objetivo_total { get; set; }
+        public IEnumerable<mdl_Dashboard_Objetivo_Total> objetivo_cartera { get; set; }
+        public IEnumerable<mdl_Dashboard_Objetivo_Total> objetivo_responsable { get; set; }
+    }
+}
