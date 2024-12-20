17ae6dfdbc223d7e663d6aeec02904172f6dd1ea
gpeol91 <93731144+gpeol91@users.noreply.github.com>
2024-12-20 10:53:08 -0700

    Actualizacion#

---
+++ HDBackend/HD_Cobranza/Capturas/Dashboard/AD_Dashboard_Proyecciones.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using HD.AccesoDatos;
+using HD_Cobranza.Modelos.Dashboard;
+using System.Data;
+
+namespace HD_Cobranza.Capturas.Dashboard
+{
+    public class AD_Dashboard_Proyecciones
+    {
+        private string CadenaConexion;
+        public AD_Dashboard_Proyecciones(string _cadenaconexion)
+        {
+            CadenaConexion = _cadenaconexion;
+        }
+        public async Task<mdlProyeccionRecuperarResult> ObtenerProyecciones(int ejercicio, int periodo)
+        {
+            try
+            {
+                FactoryConection factory = new FactoryConection(CadenaConexion);
+                var parametros = new
+                {
+                    ejercicio = ejercicio,
+                    periodo = periodo
+                };
+
+                var result = await factory.SQL.QueryMultipleAsync("Cartera_Clientes.Cobranza.sp_Dashboard_Proyecciones", parametros, commandType: System.Data.CommandType.StoredProcedure);
+                var view = new mdlProyeccionRecuperarResult();
+                view.listado = result.Read<mdlProyeccionRecuperar>().ToList();
+                view.columnas=result.Read<string>().FirstOrDefault();
+                view.tipo_cartera=result.Read<string>().FirstOrDefault();
+                factory.SQL.Close();
+                return view;
+            }
+            catch (System.Exception ex)
+            {
+                throw new Excepciones(System.Net.HttpStatusCode.InternalServerError, new { Mensaje = ex.Message });
+            }
+        }
+    }
+}
