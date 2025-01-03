e07b77e6303face554d31c1c77c53690d4086d81
SistemasSinaloa <sistemassinaloa@humaya.com.mx>
2025-01-03 13:46:17 -0700

    actualizacion menu refacturacion

---
+++ HDBackend/HD_Clientes/Modelos/Refacturacion_Credito/mdlSolicitudes_Aceptadas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HD.Clientes.Modelos.Refacturacion_Credito
+{
+    public class mdlSolicitudes_Aceptadas
+    {
+        public string? folio { get; set; }
+        public string? sucursal { get; set; }
+        public string? cliente { get; set; }
+        public string? tipo_credito { get; set; }
+        public string? vendedor { get; set; }
+        public string? fecha { get; set; }
+    }
+}
