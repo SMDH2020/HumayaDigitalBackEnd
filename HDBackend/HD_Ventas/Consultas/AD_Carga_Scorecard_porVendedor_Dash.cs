a2d74b95ca389e34f9f7e677577bf12f49c38c64
SistemasSinaloa <sistemassinaloa@humaya.com.mx>
2025-01-17 10:56:03 -0700

    Update AD_Carga_Scorecard_porVendedor_Dash.cs

--- HDBackend/HD_Ventas/Consultas/AD_Carga_Scorecard_porVendedor_Dash.cs
+++ HDBackend/HD_Ventas/Consultas/AD_Carga_Scorecard_porVendedor_Dash.cs
@@ -23,7 +23,41 @@ namespace HD_Ventas.Consultas
                 FactoryConection factory = new FactoryConection(CadenaConexion);
                 IEnumerable<mdlCarga_Scorecard_porVendedor_Dash> result = await factory.SQL.QueryAsync<mdlCarga_Scorecard_porVendedor_Dash>("Ventas.Obtener_Scorecard_porUsuario", parametros, commandType: System.Data.CommandType.StoredProcedure);
                 factory.SQL.Close();
-                return result;
+
+                var tiposFaltantes = new List<string> { "Autoguiado", "Drones", "Implementos", "Jardineros", "Producto Aliado", "Tractores", "Tractores Usados", "Trilladoras Usadas" };
+
+                // Asegurarse de que siempre haya un objeto por cada tipo de "tipo_cartera"
+                var resultConTiposCompletos = tiposFaltantes.Select(tipo =>
+                {
+                    // Buscar el tipo en los datos obtenidos
+                    var tipoExistente = result.FirstOrDefault(x => x.linea == tipo);
+
+                    if (tipoExistente != null)
+                    {
+                        // Si el tipo ya existe, devolverlo tal como está
+                        return tipoExistente;
+                    }
+                    else
+                    {
+                        // Si no existe el tipo, crear un objeto nuevo con todos los meses en 0
+                        return new mdlCarga_Scorecard_porVendedor_Dash
+                        {
+                            linea = tipo,
+                            objetivo = 0,
+                            unidades_vendidas = 0,
+                            importe = 0,
+                            importe_proyectado = 0,
+                            porcentaje = 0,
+                            objetivo_acumulado = 0,
+                            unidades_vendidas_acumulado = 0,
+                            importe_acumulado = 0,
+                            importe_proyectado_acumulado = 0,
+                            porcentaje_acumulado = 0
+                        };
+                    }
+                }).ToList();
+
+                return resultConTiposCompletos;
             }
             catch (System.Exception ex)
             {
