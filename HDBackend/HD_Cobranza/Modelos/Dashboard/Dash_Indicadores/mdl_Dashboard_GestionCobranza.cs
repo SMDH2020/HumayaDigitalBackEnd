2879d9730a8f71208b59ee16f340e58576094cf9
Desarrolladorti2 <desarrolladorti2@humaya.com.mx>
2025-02-13 09:29:27 -0700

    Actualizacion Back

--- HDBackend/HD_Cobranza/Modelos/Dashboard/Dash_Indicadores/mdl_Dashboard_GestionCobranza.cs
+++ HDBackend/HD_Cobranza/Modelos/Dashboard/Dash_Indicadores/mdl_Dashboard_GestionCobranza.cs
@@ -9,6 +9,12 @@
         public int clientes_recuperados { get; set; }
         public double conveniado { get; set; }
         public int clientes_conveniados { get; set; }
+        public float gestionado { get; set; }
+        public float gestionado_pagado {  get; set; }
+        public float gestionado_nopagado {  get; set; }
+        public int clientes_gestionados {  get; set; }
+        public int clientes_gestionados_pagados {  get; set; }
+        public int clientes_gestionados_nopagados { get; set; }
         public double pendiente { get; set; }
         public int clientes_pendientes { get; set; }
         public double total_objetivo { get; set; }
