0c5af39388a06b50e2951bde3500961413f34c89
SistemasSinaloa <sistemassinaloa@humaya.com.mx>
2025-02-13 18:05:38 -0700

    actualizacion lista de precios

---
+++ HDBackend/HD_Endpoints/Controllers/Usados/Inventario/InventarioController.cs
@@ -0,0 +1,41 @@
+﻿using HD.Clientes.Consultas.ClientesCultivo;
+using HD.Clientes.Modelos;
+using HD.Security;
+using Microsoft.AspNetCore.Mvc;
+using Usados.Modelos.Usados;
+
+namespace HD.Endpoints.Controllers.Usados.Inventario
+{
+    public class InventarioController : MyBase
+    {
+        private readonly IConfiguration Configuracion;
+        private readonly ISesion Sesion;
+        public InventarioController(IConfiguration configuration, ISesion sesion)
+        {
+            Configuracion = configuration;
+            Sesion = sesion;
+        }
+
+        [HttpGet]
+        [Route("/api/[controller]/[action]")]
+        public async Task<ActionResult> Listado(string Modelo, int ejercicio, string HP, string Sucursal, string Promocion, string Estatus)
+        {
+            string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
+            AD_Inventario_Listado datos = new AD_Inventario_Listado(CadenaConexion);
+            var result = await datos.Listado(Modelo, ejercicio, HP, Sucursal, Promocion, Estatus);
+            return Ok(result);
+
+        }
+
+        [HttpGet]
+        [Route("/api/[controller]/[action]")]
+        public async Task<ActionResult> ListadoFiltro()
+        {
+            string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
+            AD_Inventario_Listado datos = new AD_Inventario_Listado(CadenaConexion);
+            var result = await datos.ListadoFiltro();
+            return Ok(result);
+
+        }
+    }
+}
---
+++ HDBackend/Usados/Modelos/Inventario/AD_Inventario_Listado.cs
@@ -0,0 +1,61 @@
+﻿using Dapper;
+using HD.AccesoDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Usados.Consultas.Usados;
+
+namespace Usados.Modelos.Usados
+{
+    public class AD_Inventario_Listado
+    {
+        private string CadenaConexion;
+        public AD_Inventario_Listado(string _cadenaconexion)
+        {
+            CadenaConexion = _cadenaconexion;
+        }
+        public async Task<IEnumerable<mdl_Inventario>> Listado(string Modelo, int ejercicio, string HP, string Sucursal, string Promocion, string Estatus)
+        {
+            try
+            {
+                var parametros = new
+                {
+                    Modelo = Modelo,
+                    ejercicio = ejercicio,
+                    HP = HP,
+                    Sucursal = Sucursal,
+                    Promocion = Promocion,
+                    Estatus = Estatus
+                };
+                FactoryConection factory = new FactoryConection(CadenaConexion);
+                IEnumerable<mdl_Inventario> result = await factory.SQL.QueryAsync<mdl_Inventario>("Usados.sp_Inventario_Listado", parametros, commandType: System.Data.CommandType.StoredProcedure);
+                factory.SQL.Close();
+                return result;
+            }
+            catch (System.Exception ex)
+            {
+                throw new Excepciones(System.Net.HttpStatusCode.InternalServerError, new { Mensaje = ex.Message });
+            }
+        }
+
+        public async Task<IEnumerable<mdl_Inventario>> ListadoFiltro()
+        {
+            try
+            {
+                var parametros = new
+                {
+                };
+                FactoryConection factory = new FactoryConection(CadenaConexion);
+                IEnumerable<mdl_Inventario> result = await factory.SQL.QueryAsync<mdl_Inventario>("Usados.sp_Inventario_Listado_Filtros", parametros, commandType: System.Data.CommandType.StoredProcedure);
+                factory.SQL.Close();
+                return result;
+            }
+            catch (System.Exception ex)
+            {
+                throw new Excepciones(System.Net.HttpStatusCode.InternalServerError, new { Mensaje = ex.Message });
+            }
+        }
+    }
+}
