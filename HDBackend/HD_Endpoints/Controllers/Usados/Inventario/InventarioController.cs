4de16b824c66e1ef7be7ac453ecb4368004a8afb
SistemasSinaloa <sistemassinaloa@humaya.com.mx>
2025-03-06 16:01:46 -0700

    actualizacion mobile

--- HDBackend/HD_Endpoints/Controllers/Usados/Inventario/InventarioController.cs
+++ HDBackend/HD_Endpoints/Controllers/Usados/Inventario/InventarioController.cs
@@ -80,6 +80,16 @@ namespace HD.Endpoints.Controllers.Usados.Inventario
             return Ok(result);
         }
 
+        [HttpGet]
+        [Route("/api/[controller]/[action]")]
+        public async Task<ActionResult> ListadoFiltroMovil()
+        {
+            string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
+            AD_Inventario_Listado datos = new AD_Inventario_Listado(CadenaConexion);
+            var result = await datos.ListadoFiltroMovil();
+            return Ok(result);
+        }
+
         [HttpPost]
         [Route("/api/[controller]/[action]")]
         public async Task<ActionResult> ImprimirPDF(IEnumerable<mdl_Inventario> mdl)
@@ -116,6 +126,30 @@ namespace HD.Endpoints.Controllers.Usados.Inventario
             try
             {
                 RPT_Result documento = RPT_Listado_Precios_Corto.GenerarPDF(mdl);
+
+                return Ok(documento);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Error de servidor");
+
+            }
+        }
+
+        [HttpPost]
+        [Route("/api/[controller]/[action]")]
+        public async Task<ActionResult> ImprimirListadoPDFMovil()
+        {
+            // Concatenar todos los idinventario en una cadena separada por comas
+            //string idinventario = string.Join(",", mdl.datosActualizados.Select(r => r.idinventario.ToString()));
+
+            string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
+            AD_Inventario_Listado datos = new AD_Inventario_Listado(CadenaConexion);
+            var result = await datos.ListadoFiltro();
+
+            try
+            {
+                RPT_Result documento = RPT_Listado_Precios_Corto.GenerarPDF(result);
 
                 return Ok(documento);
             }
