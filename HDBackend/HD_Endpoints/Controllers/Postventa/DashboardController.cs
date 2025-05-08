a914ac2ab2bfd7bc01e991599643fe1d3c103590
SistemasSinaloa <sistemassinaloa@humaya.com.mx>
2025-05-08 09:22:35 -0700

    actualizacion postventa

---
+++ HDBackend/HD_Endpoints/Controllers/Postventa/DashboardController.cs
@@ -0,0 +1,29 @@
+﻿using HD.Security;
+using HD_Cobranza.GestionCobranza.Capturas;
+using HD_Cobranza.GestionCobranza.Modelos;
+using Microsoft.AspNetCore.Mvc;
+using Postventa.Consultas.Dashboard;
+
+namespace HD.Endpoints.Controllers.Postventa
+{
+    public class DashboardPostventaController : MyBase
+    {
+        private readonly IConfiguration Configuracion;
+        private readonly ISesion Sesion;
+        public DashboardPostventaController(IConfiguration configuration, ISesion sesion)
+        {
+            Configuracion = configuration;
+            Sesion = sesion;
+        }
+
+        [HttpGet]
+        [Route("/api/[controller]/[action]")]
+        public async Task<ActionResult> Dashboard(int ejercicio, int periodo_inicio, int periodo_fin, string adr, string sucursal)
+        {
+            string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
+            AD_Dashboard_Postventa_Info datos = new AD_Dashboard_Postventa_Info(CadenaConexion);
+            var result = await datos.ObtenerDashboard(ejercicio, periodo_inicio, periodo_fin, adr, sucursal);
+            return Ok(result);
+        }
+    }
+}
---
+++ HDBackend/Postventa/Modelos/mdl_Dashboard_View.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Postventa.Modelos
+{
+    public class mdl_Dashboard_View
+    {
+        public IEnumerable<mdl_Dashboard_Servicio>? servicio { get; set; }
+        //public string? tipo_cartera { get; set; }
+        //public string? columnas { get; set; }
+    }
+}
