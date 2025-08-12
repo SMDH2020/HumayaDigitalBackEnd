6e4a866ea128d5c38aa047a5cf5baab7dff3712d
SistemasSinaloa <sistemassinaloa@humaya.com.mx>
2025-08-12 10:15:42 -0700

    convertir imagenes a PDF

---
+++ HDBackend/HD_Reporteria/ImagentoPDF/mdl_Covertor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HD_Reporteria.ImagentoPDF
+{
+    public class mdl_Covertor
+    {
+        public List<string> ImagenesBase64 { get; set; }
+
+    }
+}
