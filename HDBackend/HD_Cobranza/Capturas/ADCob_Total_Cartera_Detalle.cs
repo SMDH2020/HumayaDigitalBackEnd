c4827b7b4c9660bf0b2f9977b18054f840f59739
SistemasSinaloa <sistemassinaloa@humaya.com.mx>
2025-09-23 12:36:42 -0700

    actualizacion

---
+++ HDBackend/HD_Cobranza/Capturas/ADCob_Total_Cartera_Detalle.cs
@@ -0,0 +1,51 @@
+﻿using Dapper;
+using HD.AccesoDatos;
+using HD_Cobranza.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HD_Cobranza.Capturas
+{
+    public class ADCob_Total_Cartera_Detalle
+    {
+        private string CadenaConexion;
+        public ADCob_Total_Cartera_Detalle(string _cadenaconexion)
+        {
+            CadenaConexion = _cadenaconexion;
+        }
+        public async Task<IEnumerable<mdlCob_Total_Cartera_Detalle>> Listado(string adr, string sucursal, int ejercicio, int periodo, string linea, string usuario)
+        {
+            if(adr == "0")
+            {
+                adr = "";
+            }
+
+            if(sucursal == "0")
+            {
+                sucursal = "100";
+            }
+            try
+            {
+                FactoryConection factory = new FactoryConection(CadenaConexion);
+                var parametros = new
+                {
+                    region = adr,
+                    sucursales = sucursal,
+                    lineas = linea,
+                    usuario = 1
+                };
+                var result = await factory.SQL.QueryAsync<mdlCob_Total_Cartera_Detalle>("EQUIP.Credito.sp_Obtener_TotalCartera_Detalle", parametros, commandType: System.Data.CommandType.StoredProcedure);
+                factory.SQL.Close();
+
+                return result;
+            }
+            catch (System.Exception ex)
+            {
+                throw new Excepciones(System.Net.HttpStatusCode.InternalServerError, new { Mensaje = ex.Message });
+            }
+        }
+    }
+}
--- HDBackend/HD_Endpoints/Controllers/Cobranza/TotalCarteraPorSucursalController.cs
+++ HDBackend/HD_Endpoints/Controllers/Cobranza/TotalCarteraPorSucursalController.cs
@@ -28,6 +28,18 @@ namespace HD.Endpoints.Controllers.Cobranza
             return Ok(result);
 
         }
+
+        [HttpGet]
+        [Route("/api/[controller]/[action]")]
+        public async Task<ActionResult> ListadoDetalle(string adr, string sucursal, int ejercicio, int periodo, string linea)
+        {
+            string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
+            ADCob_Total_Cartera_Detalle datos = new ADCob_Total_Cartera_Detalle(CadenaConexion);
+            string usuario = Sesion.usuario();
+            var result = await datos.Listado(adr, sucursal, ejercicio, periodo, linea, usuario);
+            return Ok(result);
+
+        }
         [HttpGet]
         [Route("/api/[controller]/[action]")]
         public async Task<ActionResult> GenerarExcel(string adr, string sucursal)
@@ -39,6 +51,7 @@ namespace HD.Endpoints.Controllers.Cobranza
             var docresult = await XLSCob_TotalCartera_Sucursal.CrearResumenPorSucursal(result);
             return Ok(docresult);
         }
+
 
         [HttpGet]
         [Route("/api/[controller]/[action]")]
