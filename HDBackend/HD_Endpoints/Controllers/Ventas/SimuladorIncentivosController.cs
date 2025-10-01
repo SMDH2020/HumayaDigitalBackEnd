53c094499f1e156e2613fab4366cfb1efca90fc3
gpeol91 <93731144+gpeol91@users.noreply.github.com>
2025-09-30 17:49:35 -0700

    Simulador 30092025

---
+++ HDBackend/HD_Endpoints/Controllers/Ventas/SimuladorIncentivosController.cs
@@ -0,0 +1,29 @@
+﻿using HD.Security;
+using HD_Ventas.Consultas;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HD.Endpoints.Controllers.Ventas
+{
+    public class SimuladorIncentivosController : MyBase
+    {
+        private readonly IConfiguration Configuracion;
+        private readonly ISesion Sesion;
+        public SimuladorIncentivosController(IConfiguration configuration, ISesion sesion)
+        {
+            Configuracion = configuration;
+            Sesion = sesion;
+        }
+
+        [HttpGet]
+        [Route("/api/[controller]/[action]")]
+        public async Task<ActionResult> Obtener_Roles()
+        {
+            string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
+            AD_Simulador_Incentivos datos = new AD_Simulador_Incentivos(CadenaConexion);
+            int usuario = int.Parse(Sesion.usuario());
+            //usuario = 5630;
+            var result = await datos.Obtener_Roles_Usuario(usuario);
+            return Ok(result);
+        }
+    }
+}
---
+++ HDBackend/HD_Ventas/Consultas/AD_Simulador_Incentivos.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using HD.AccesoDatos;
+using HD_Ventas.Modelos;
+
+namespace HD_Ventas.Consultas
+{
+    public class AD_Simulador_Incentivos
+    {
+        private string CadenaConexion;
+        public AD_Simulador_Incentivos(string _cadenaconexion)
+        {
+            CadenaConexion = _cadenaconexion;
+        }
+        public async Task<IEnumerable<mdlSimulador_Roles_Usuario>> Obtener_Roles_Usuario(int usuario)
+        {
+            try
+            {
+                var parametros = new
+                {
+                    idusuario = usuario
+                };
+                FactoryConection factory = new FactoryConection(CadenaConexion);
+                IEnumerable<mdlSimulador_Roles_Usuario> result = await factory.SQL.QueryAsync<mdlSimulador_Roles_Usuario>("sp_obtener_roles_simulador", parametros, commandType: System.Data.CommandType.StoredProcedure);
+                factory.SQL.Close();
+                return result;
+            }
+            catch (System.Exception ex)
+            {
+                throw new Excepciones(System.Net.HttpStatusCode.InternalServerError, new { Mensaje = ex.Message });
+            }
+        }
+    }
+}
