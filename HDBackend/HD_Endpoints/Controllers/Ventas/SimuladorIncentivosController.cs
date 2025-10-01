fac984956c4b92e21f8c51c2d0378113e1d5a7b2
gpeol91 <93731144+gpeol91@users.noreply.github.com>
2025-10-01 10:02:43 -0700

    Actualizaciones

--- HDBackend/HD_Endpoints/Controllers/Ventas/SimuladorIncentivosController.cs
+++ HDBackend/HD_Endpoints/Controllers/Ventas/SimuladorIncentivosController.cs
@@ -1,5 +1,6 @@
 ﻿using HD.Security;
 using HD_Ventas.Consultas;
+using HD_Ventas.Modelos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HD.Endpoints.Controllers.Ventas
@@ -23,6 +24,27 @@ namespace HD.Endpoints.Controllers.Ventas
             int usuario = int.Parse(Sesion.usuario());
             //usuario = 5630;
             var result = await datos.Obtener_Roles_Usuario(usuario);
+            return Ok(result);
+        }
+        [HttpGet]
+        [Route("/api/[controller]/[action]")]
+        public async Task<ActionResult> Obtener_documento(string idrol)
+        {
+            string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
+            AD_Simulador_Incentivos datos = new AD_Simulador_Incentivos(CadenaConexion);
+
+            var result = await datos.Obtener_Documento(idrol);
+            return Ok(result);
+        }
+        [HttpPost]
+        [Route("/api/[controller]/[action]")]
+        public async Task<ActionResult> Guardar_documento(mdlSimulador_documento_guardar obj)
+        {
+            string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
+            AD_Simulador_Incentivos datos = new AD_Simulador_Incentivos(CadenaConexion);
+            obj.usuario = Sesion.usuario();
+            //usuario = 5630;
+            var result = await datos.Guardar_Documento(obj);
             return Ok(result);
         }
     }
--- HDBackend/HD_Ventas/Consultas/AD_Simulador_Incentivos.cs
+++ HDBackend/HD_Ventas/Consultas/AD_Simulador_Incentivos.cs
@@ -29,5 +29,47 @@ namespace HD_Ventas.Consultas
                 throw new Excepciones(System.Net.HttpStatusCode.InternalServerError, new { Mensaje = ex.Message });
             }
         }
+
+        public async Task<object> Guardar_Documento(mdlSimulador_documento_guardar obj)
+        {
+            try
+            {
+                var parametros = new
+                {
+                    idrol=obj.idrol,
+                    documento=obj.idrol,
+                    idusuario = obj.usuario
+                };
+                FactoryConection factory = new FactoryConection(CadenaConexion);
+                await factory.SQL.QueryAsync<mdlSimulador_documento_obtener>("sp_simulador_incentivo_documento_guardar", parametros, commandType: System.Data.CommandType.StoredProcedure);
+                factory.SQL.Close();
+                return new
+                {
+                    mensaje="Documento cargado con exito"
+                };
+            }
+            catch (System.Exception ex)
+            {
+                throw new Excepciones(System.Net.HttpStatusCode.InternalServerError, new { Mensaje = ex.Message });
+            }
+        }
+        public async Task<IEnumerable<mdlSimulador_documento_obtener>> Obtener_Documento(string idrol)
+        {
+            try
+            {
+                var parametros = new
+                {
+                    idrol = idrol
+                };
+                FactoryConection factory = new FactoryConection(CadenaConexion);
+                IEnumerable<mdlSimulador_documento_obtener> result = await factory.SQL.QueryAsync<mdlSimulador_documento_obtener>("sp_simulador_incentivo_documento_obtener", parametros, commandType: System.Data.CommandType.StoredProcedure);
+                factory.SQL.Close();
+                return result;
+            }
+            catch (System.Exception ex)
+            {
+                throw new Excepciones(System.Net.HttpStatusCode.InternalServerError, new { Mensaje = ex.Message });
+            }
+        }
     }
 }
