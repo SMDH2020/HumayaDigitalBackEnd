9dd9aee47f27778bb9fa8d1292cf2671bcfe921c
gpeol91 <93731144+gpeol91@users.noreply.github.com>
2025-12-01 12:41:21 -0700

    actualizacion

--- HDBackend/HD_Notifications/Consultas/AD_HD_Notificaciones_Enviar_Push.cs
+++ HDBackend/HD_Notifications/Consultas/AD_HD_Notificaciones_Enviar_Push.cs
@@ -18,7 +18,7 @@ namespace HD.Notifications.Consultas
 
         private const string OneSignalAppId = "04e611d6-045a-4105-af2d-04880d3c4cb9"; // Tu App ID
 
-        private const string OneSignalApiKey = "os_v2_app_attbdvqeljaqllznasea2pcmxg3bivslzcpur34bx6g5g6i56rzp3ajx44oerbkx77useti2vmimfrjo636cikgj3axrcrqze4offja"; // ⚠️ Tu REST API Key
+        private const string OneSignalApiKey = "os_v2_app_attbdvqeljaqllznasea2pcmxekuoyv7pueeg654qgf6zciqakxbbayf3wf2gtrvflz6rawthlg3kik5dca2f7kwyfzk5pskekkeu4a"; // ⚠️ Tu REST API Key
 
 
         public async Task<bool> Enviar_Notificacion_Solicitud(mdl_Notificacion_Usuarios_Solicitudes_View mdl, string? titulo)
