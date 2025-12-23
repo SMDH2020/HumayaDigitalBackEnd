1642a660b9ccef5c430f1a9e106f049bf08c0aae
SistemasSinaloa <sistemassinaloa@humaya.com.mx>
2025-12-23 11:30:25 -0700

    Actualizacion credito
    - CURP opcional
    - Pantalla de relacion de documentos
    - Correccion de contado

---
+++ HDBackend/HD_Clientes/Consultas/RelacionDocumentosSC/AD_RelacionDocumentosSC_Guardar.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using HD.AccesoDatos;
+using HD.Clientes.Modelos.RelacionDocumentosSC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HD.Clientes.Consultas.RelacionDocumentosSC
+{
+    public class AD_RelacionDocumentosSC_Guardar
+    {
+        private string CadenaConexion;
+        public AD_RelacionDocumentosSC_Guardar(string _cadenaconexion)
+        {
+            CadenaConexion = _cadenaconexion;
+        }
+        public async Task<bool> Guardar(mdl_RelacionDocumentosSC_Guardar mdl, string usuario)
+        {
+
+            try
+            {
+                FactoryConection factory = new FactoryConection(CadenaConexion);
+                var parametros = new
+                {
+                    detalle = mdl.detalle,
+                    usuario = usuario
+                };
+                await factory.SQL.QueryAsync("Credito.sp_Rel_Documentos_SC_Guardar", parametros, commandType: System.Data.CommandType.StoredProcedure);
+                factory.SQL.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new Excepciones(System.Net.HttpStatusCode.InternalServerError, new { Mensaje = ex.Message });
+            }
+        }
+    }
+}
