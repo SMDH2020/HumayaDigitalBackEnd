ab4d39c66cb87ca82c4e47a3c1f26f682dca9e16
DesarrolladorMhusa3 <desarrolladorti3@humaya.com.mx>
2026-03-31 12:32:26 -0700

    Version 1 Seguimiento Actividades

---
+++ HDBackend/HD_GestionActividades/Consultas/SalaActividad/AD_SalaActividad.cs
@@ -0,0 +1,86 @@
+﻿using Dapper;
+using HD.AccesoDatos;
+using HD_GestionActividades.Modelos;
+
+namespace HD_GestionActividades.Consultas.SalaActividad
+{
+    public class AD_SalaActividad
+    {
+        private string CadenaConexion;
+
+        public AD_SalaActividad(string _cadenaconexion)
+        {
+            CadenaConexion = _cadenaconexion;
+        }
+
+
+        public async Task Guardar(short idSala, short idActividad, short idUsuario)
+        {
+            FactoryConection factory = new FactoryConection(CadenaConexion);
+
+            var parametros = new
+            {
+                idSala,
+                idActividad,
+                idUsuario
+            };
+
+            await factory.SQL.ExecuteAsync(
+                "Seguimiento_Actividades..SP_Rel_Sala_Actividad_Guardar",
+                parametros,
+                commandType: System.Data.CommandType.StoredProcedure
+            );
+
+            factory.SQL.Close();
+        }
+
+        public async Task<List<dynamic>> Listado(short idSala)
+        {
+            FactoryConection factory = new FactoryConection(CadenaConexion);
+
+            var parametros = new
+            {
+                idSala
+            };
+
+            var result = await factory.SQL.QueryAsync(
+                "Seguimiento_Actividades..SP_Rel_Sala_Actividad_Listado",
+                parametros,
+                commandType: System.Data.CommandType.StoredProcedure
+            );
+
+            factory.SQL.Close();
+            return result.ToList();
+        }
+
+
+        public async Task<IEnumerable<dynamic>> EliminarPorID(int idRelSalaActividad, int user, int idSala)
+        {
+            FactoryConection factory = new FactoryConection(CadenaConexion);
+            try
+            {
+                var parametros = new
+                {
+                    idRelSalaActividad,
+                    user,
+                    idSala
+                };
+
+                var result = await factory.SQL.QueryAsync(
+                    "Seguimiento_Actividades..SP_Rel_Sala_Actividad_EliminarPorID",
+                    parametros,
+                    commandType: System.Data.CommandType.StoredProcedure
+                );
+
+                factory.SQL.Close();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return new List<dynamic>();
+        }
+    }
+}
---
+++ HDBackend/HD_Notifications/Modelos/mdlSeguimiento_Email.cs
@@ -0,0 +1,8 @@
+﻿public class mdlSeguimiento_Email
+{
+    public int idSala { get; set; }  
+    public string? actividad { get; set; }
+    public string? comentarios { get; set; }
+    public string? estatus { get; set; }
+    public string? usuario { get; set; }
+}
