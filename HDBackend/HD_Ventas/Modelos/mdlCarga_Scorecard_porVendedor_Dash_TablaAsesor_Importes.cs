3cc0a2677dc99a6dfe5f03f7b80e1711d09a1489
SistemasSinaloa <102257158+SistemasSinaloa@users.noreply.github.com>
2026-04-10 18:00:35 -0700

    act

--- HDBackend/HD_Ventas/Modelos/mdlCarga_Scorecard_porVendedor_Dash_TablaAsesor_Importes.cs
+++ HDBackend/HD_Ventas/Modelos/mdlCarga_Scorecard_porVendedor_Dash_TablaAsesor_Importes.cs
@@ -14,24 +14,51 @@ namespace HD_Ventas.Modelos
         public string? asesor { get; set; }
         public double Objetivo_Autoguiados { get; set; }
         public double Real_Autoguiados { get; set; }
+        public double Comp_Autoguiados { get; set; }
+
         public double Objetivo_Drones { get; set; }
         public double Real_Drones { get; set; }
+        public double Comp_Drones { get; set; }
+
+
         public double Objetivo_Implementos { get; set; }
         public double Real_Implementos { get; set; }
+        public double comp_Implementos { get; set; }
+
+
         public double Objetivo_Jardineros { get; set; }
         public double Real_Jardineros { get; set; }
+        public double Comp_Jardineros { get; set; }
+
+
         public double Objetivo_PA { get; set; }
         public double Real_PA { get; set; }
+        public double Comp_PA { get; set; }
+
+
         public double Objetivo_Tractores { get; set; }
         public double Real_Tractores { get; set; }
+        public double Comp_Tractores { get; set; }
+
+
         public double Objetivo_TracUsa { get; set; }
         public double Real_TracUsa { get; set; }
+        public double Comp_TracUsa { get; set; }
+
+
         public double Objetivo_TriUsa { get; set; }
         public double Real_TriUsa { get; set; }
+        public double Comp_TriUsa { get; set; }
+
 
         public double Objetivo_Garantia { get; set; }
         public double Real_Garantia { get; set; }
+        public double Comp_Garantia { get; set; }
+
+
         public double Objetivo_Mantenimiento { get; set; }
         public double Real_Mantenimiento { get; set; }
+        public double Comp_Poliza { get; set; }
+
     }
 }
