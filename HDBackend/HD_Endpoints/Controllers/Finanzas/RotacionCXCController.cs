c3b24ee70fb56d6257d98e21b45bd565d6fbf521
SistemasSinaloa <102257158+SistemasSinaloa@users.noreply.github.com>
2026-04-13 17:23:02 -0700

    actualizacion

---
+++ HDBackend/HD_Endpoints/Controllers/Finanzas/RotacionCXCController.cs
@@ -0,0 +1,39 @@
+﻿using HD.Security;
+using HD_Finanzas.AccesoDatos.RotacionCXC;
+using HD_Finanzas.AccesoDatos.RotacionInventario;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HD.Endpoints.Controllers.Finanzas
+{
+    public class RotacionCXCController : MyBase
+    {
+        private readonly IConfiguration Configuracion;
+        private readonly ISesion Sesion;
+
+        public RotacionCXCController(IConfiguration configuration, ISesion sesion)
+        {
+            Configuracion = configuration;
+            Sesion = sesion;
+        }
+
+        //[HttpPost]
+        //[Route("/api/[controller]/[action]")]
+        //public async Task<ActionResult> GuardarGuiaCXC(int ejercicio)
+        //{
+        //    string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
+        //    GuiaCXCListado datos = new GuiaCXCListado(CadenaConexion);
+        //    var result = await datos.Listado(ejercicio);
+        //    return Ok(result);
+        //}
+
+        [HttpGet]
+        [Route("/api/[controller]/[action]")]
+        public async Task<ActionResult> ListadoGuiaCXC(int ejercicio)
+        {
+            string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
+            GuiaCXCListado datos = new GuiaCXCListado(CadenaConexion);
+            var result = await datos.Listado(ejercicio);
+            return Ok(result);
+        }
+    }
+}
---
+++ HDBackend/HD_Finanzas/Modelos/RotacionInventario/mdl_Rotacion_CXC.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HD_Finanzas.Modelos.RotacionInventario
+{
+    public class mdl_Rotacion_CXC
+    {
+        public float cxc { get; set; }
+
+    }
+}
