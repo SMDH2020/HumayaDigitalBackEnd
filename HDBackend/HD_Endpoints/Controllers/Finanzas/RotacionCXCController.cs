65b12552c453fdd28d46374114b2b0d49c62caba
SistemasSinaloa <102257158+SistemasSinaloa@users.noreply.github.com>
2026-04-14 17:19:34 -0700

    actualizacion finanzas CXC

--- HDBackend/HD_Endpoints/Controllers/Finanzas/RotacionCXCController.cs
+++ HDBackend/HD_Endpoints/Controllers/Finanzas/RotacionCXCController.cs
@@ -1,6 +1,7 @@
 ﻿using HD.Security;
 using HD_Finanzas.AccesoDatos.RotacionCXC;
 using HD_Finanzas.AccesoDatos.RotacionInventario;
+using HD_Finanzas.Modelos.RotacionCXC;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HD.Endpoints.Controllers.Finanzas
@@ -16,15 +17,28 @@ namespace HD.Endpoints.Controllers.Finanzas
             Sesion = sesion;
         }
 
-        //[HttpPost]
-        //[Route("/api/[controller]/[action]")]
-        //public async Task<ActionResult> GuardarGuiaCXC(int ejercicio)
-        //{
-        //    string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
-        //    GuiaCXCListado datos = new GuiaCXCListado(CadenaConexion);
-        //    var result = await datos.Listado(ejercicio);
-        //    return Ok(result);
-        //}
+
+        [HttpGet]
+        [Route("/api/[controller]/[action]")]
+        public async Task<ActionResult> ReporteCXC(int ejercicio, int periodo, string adr, string sucursales)
+        {
+            string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
+            AD_RotacionCXC_Reporte datos = new AD_RotacionCXC_Reporte(CadenaConexion);
+            var usuario = Sesion.usuario();
+            var result = await datos.reporte(ejercicio, periodo, adr, sucursales, usuario);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("/api/[controller]/[action]")]
+        public async Task<ActionResult> GuardarGuiaCXC(mdl_GuiaCXC_Guardar mdl)
+        {
+            string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
+            AD_GuiaCXC_Guardar datos = new AD_GuiaCXC_Guardar(CadenaConexion);
+            mdl.usuario = Sesion.usuario();
+            var result = await datos.Guardar(mdl);
+            return Ok(result);
+        }
 
         [HttpGet]
         [Route("/api/[controller]/[action]")]
---
+++ HDBackend/HD_Finanzas/Modelos/RotacionInventario/mdl_RotacionCXC.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HD_Finanzas.Modelos.RotacionInventario
+{
+    public class mdl_RotacionCXC
+    {
+        public int idlinea { get; set; }
+        public string? linea { get; set; }
+        public int saldo_inicial { get; set; }
+        public int movimiento { get; set; }
+        public int saldo_final { get; set; }
+        public int rotacion { get; set; }
+        public int guia { get; set; }
+         
+    }
+}
