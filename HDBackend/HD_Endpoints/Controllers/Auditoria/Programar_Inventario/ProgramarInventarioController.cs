b32546dd2b1cf338e44ff57f3841d810e67353d0
SistemasSinaloa <102257158+SistemasSinaloa@users.noreply.github.com>
2026-05-14 08:59:13 -0700

    actualixacion Auditoria
    - modal de resposables de sucursal
    - modal para extender fecha
    - modal para auditores adicionales
    - eliminar una programacion

--- HDBackend/HD_Endpoints/Controllers/Auditoria/Programar_Inventario/ProgramarInventarioController.cs
+++ HDBackend/HD_Endpoints/Controllers/Auditoria/Programar_Inventario/ProgramarInventarioController.cs
@@ -28,6 +28,7 @@ namespace HD.Endpoints.Controllers.Auditoria.Programar_Inventario
             return Ok(result);
 
         }
+
         [HttpPost]
         [Route("/api/[controller]/[action]")]
         public async Task<IActionResult> Guardar(mdl_Programar_Inventario mdl)
@@ -38,5 +39,61 @@ namespace HD.Endpoints.Controllers.Auditoria.Programar_Inventario
             var result = await datos.ProgramarInventario(mdl);
             return Ok(result);
         }
+
+        [HttpPost]
+        [Route("/api/[controller]/[action]")]
+        public async Task<IActionResult> EliminarProgramacion(mdl_ExtenderFecha mdl)
+        {
+            string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
+            AD_Programar_Inventario_Eliminar datos = new AD_Programar_Inventario_Eliminar(CadenaConexion);
+            mdl.usuario = Sesion.usuario();
+            var result = await datos.EliminarRegistro(mdl);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("/api/[controller]/[action]")]
+        public async Task<IActionResult> ListadoAuditores(string? folio)
+        {
+            string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
+            AD_Programar_Inventario_Auditor_Listado datos = new AD_Programar_Inventario_Auditor_Listado(CadenaConexion);
+            var result = await datos.buscarFolio(folio);
+            return Ok(result);
+
+        }
+
+        [HttpPost]
+        [Route("/api/[controller]/[action]")]
+        public async Task<IActionResult> GuardarReasignacion(mdl_Auditores_Guardar mdl)
+        {
+            string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
+            AD_Programar_Inventario_Reasignacion_Guardar datos = new AD_Programar_Inventario_Reasignacion_Guardar(CadenaConexion);
+            mdl.usuario = Sesion.usuario();
+            var result = await datos.ProgramarInventario(mdl);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("/api/[controller]/[action]")]
+        public async Task<IActionResult> ExtenderFecha(mdl_ExtenderFecha mdl)
+        {
+            string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
+            AD_Programar_Inventario_Extender_Fecha datos = new AD_Programar_Inventario_Extender_Fecha(CadenaConexion);
+            mdl.usuario = Sesion.usuario();
+            var result = await datos.ExtenderFecha(mdl);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("/api/[controller]/[action]")]
+        public async Task<IActionResult> GuardarEncargados(mdl_Encargados mdl)
+        {
+            string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
+            AD_Encargados_Sucursal_Guardar datos = new AD_Encargados_Sucursal_Guardar(CadenaConexion);
+            mdl.usuario = Sesion.usuario();
+            var result = await datos.GuardarEncargados(mdl);
+            return Ok(result);
+        }
+
     }
 }
