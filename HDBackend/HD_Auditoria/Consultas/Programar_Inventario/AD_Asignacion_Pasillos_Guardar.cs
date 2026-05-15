7c5e65eaf562a70c3f84ae8c5f76c1e5ce5b94ec
SistemasSinaloa <102257158+SistemasSinaloa@users.noreply.github.com>
2026-05-15 09:02:06 -0700

    actualizacion auditoria

---
+++ HDBackend/HD_Auditoria/Consultas/Programar_Inventario/AD_Asignacion_Pasillos_Guardar.cs
@@ -0,0 +1,52 @@
+﻿using Dapper;
+using HD.AccesoDatos;
+using HD_Auditoria.Modelos.Programar_Inventario;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HD_Auditoria.Consultas.Programar_Inventario
+{
+    public class AD_Asignacion_Pasillos_Guardar
+    {
+        private string CadenaConexion;
+        public AD_Asignacion_Pasillos_Guardar(string _cadenaconexion)
+        {
+            CadenaConexion = _cadenaconexion;
+        }
+
+        public async Task<mdl_Result_SP> GuardarAsignaciones(mdl_AsignacionesPasillo mdl)
+        {
+            try
+            {
+
+                //Parametros de entrada
+                var parametros = new DynamicParameters();
+                parametros.Add("@folio", mdl.folio, System.Data.DbType.String, System.Data.ParameterDirection.Input);
+                parametros.Add("@asignaciones", mdl.asignaciones, System.Data.DbType.String, System.Data.ParameterDirection.Input);
+                parametros.Add("@usuario", mdl.usuario, System.Data.DbType.String, System.Data.ParameterDirection.Input);
+
+
+                //Parametros de respuesta
+                parametros.Add("@resultado", dbType: System.Data.DbType.Int32, direction: System.Data.ParameterDirection.Output);
+                parametros.Add("@mensaje", dbType: System.Data.DbType.String, direction: System.Data.ParameterDirection.Output, size: 500);
+
+
+                FactoryConection factory = new FactoryConection(CadenaConexion);
+                await factory.SQL.ExecuteAsync("Auditoria.SP_PROG_AUDITORIA_ASIGNACION_PASILLOS_GUARDAR", parametros, commandType: System.Data.CommandType.StoredProcedure);
+                factory.SQL.Close();
+                return new mdl_Result_SP
+                {
+                    resultado = parametros.Get<int>("@resultado"),
+                    mensaje = parametros.Get<string>("@mensaje")
+                };
+            }
+            catch (System.Exception ex)
+            {
+                throw new Excepciones(System.Net.HttpStatusCode.InternalServerError, new { Mensaje = ex.Message });
+            }
+        }
+    }
+}
