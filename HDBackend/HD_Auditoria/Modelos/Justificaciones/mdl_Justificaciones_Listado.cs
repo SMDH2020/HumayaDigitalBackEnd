509f6c0f84315d1fd3b72129da3899a02ee8dea2
SistemasSinaloa <102257158+SistemasSinaloa@users.noreply.github.com>
2026-05-18 16:41:40 -0700

    actualizacion auditoria
    cambios menores programacion y justificaciones

---
+++ HDBackend/HD_Auditoria/Modelos/Justificaciones/mdl_Justificaciones_Listado.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HD_Auditoria.Modelos.Justificaciones
+{
+    public class mdl_Justificaciones_Listado
+    {
+    }
+}
---
+++ HDBackend/HD_Endpoints/Controllers/Auditoria/Justificaciones/AuditoriaJustificacionesController.cs
@@ -0,0 +1,31 @@
+﻿using HD.Security;
+using HD_Auditoria.Consultas.Justificaciones;
+using HD_Auditoria.Consultas.Programar_Inventario;
+using HD_Auditoria.Modelos.Programar_Inventario;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HD.Endpoints.Controllers.Auditoria.Justificaciones
+{
+    public class AuditoriaJustificacionesController : MyBase
+    {
+        private readonly IConfiguration Configuracion;
+        private readonly ISesion Sesion;
+        public AuditoriaJustificacionesController(IConfiguration configuration, ISesion sesion)
+        {
+            Configuracion = configuration;
+            Sesion = sesion;
+        }
+
+        [HttpGet]
+        [Route("/api/[controller]/[action]")]
+        public async Task<IActionResult> Listado()
+        {
+            string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
+            AD_Justificaciones_Listado datos = new AD_Justificaciones_Listado(CadenaConexion);
+            var usuario = Sesion.usuario();
+            var result = await datos.Listado(usuario);
+            return Ok(result);
+
+        }
+    }
+}
