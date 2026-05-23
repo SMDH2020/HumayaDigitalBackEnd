89f0174dd081e714943bfece9c5bc88d09aedf46
SistemasSinaloa <102257158+SistemasSinaloa@users.noreply.github.com>
2026-05-22 17:41:21 -0700

    Update mdl_Listado_Inventario_Conteo_View.cs

--- HDBackend/HD_Auditoria/Modelos/Conteo_Piezas/mdl_Listado_Inventario_Conteo_View.cs
+++ HDBackend/HD_Auditoria/Modelos/Conteo_Piezas/mdl_Listado_Inventario_Conteo_View.cs
@@ -2,7 +2,7 @@
 {
     public class mdl_Listado_Inventario_Conteo_View
     {
-        public mdl_Listado_Inventario_Conteo_Header ? header { get; set; }
+        public mdl_Listado_Inventario_Conteo_Header? header { get; set; }
         public mdl_Listado_Inventario_Conteo_Header_KPIs? kpis { get; set; }
         public List<mdl_Listado_Inventario_Conteo_Piezas>? listado_inv { get; set; }
         public List<mdl_Posicion_Extra>? posiciones_extra { get; set; }
