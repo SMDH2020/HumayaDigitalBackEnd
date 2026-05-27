63d6bd9734edd05c0b9641b44359177e517e7493
Desarrolladorti2 <desarrolladorti2@humaya.com.mx>
2026-05-27 15:08:57 -0700

    Actualizacion Back

---
+++ HDBackend/HD_Endpoints/Controllers/Auditoria/Reporteria/ReporteSimplificadoController.cs
@@ -0,0 +1,31 @@
+﻿using HD.Security;
+using HD_Auditoria.Consultas.Conteo_Piezas;
+using HD_Auditoria.Consultas.Reporteria;
+using HD_Auditoria.Reporteria;
+using HD_Cobranza.Reportes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HD.Endpoints.Controllers.Auditoria.Reporteria
+{
+    public class ReporteSimplificadoController : MyBase
+    {
+        private readonly IConfiguration Configuracion;
+        private readonly ISesion Sesion;
+        public ReporteSimplificadoController(IConfiguration configuration, ISesion sesion)
+        {
+            Configuracion = configuration;
+            Sesion = sesion;
+        }
+
+        [HttpGet]
+        [Route("/api/[controller]/[action]")]
+        public async Task<ActionResult> GenerarExcel(string folio)
+        {
+            string CadenaConexion = Configuracion["ConnectionStrings:Servicio"];
+            AD_Reporte_Simplificado datos = new AD_Reporte_Simplificado(CadenaConexion);
+            var result = await datos.ReporteSimplificado(folio);
+            var docresult = await XLS_Reporte_Simplificado.GenerarExcel(result, folio);
+            return Ok(docresult);
+        }
+    }
+}
