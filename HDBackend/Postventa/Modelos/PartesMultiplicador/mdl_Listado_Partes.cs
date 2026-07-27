c97dd45fa13f9ab433b118dcde5c8905346382a4
SistemasSinaloa <102257158+SistemasSinaloa@users.noreply.github.com>
2026-07-27 08:41:54 -0700

    actualizacion refacciones
    Agregado de pantalla para ligar codigo de vendedores con su nombre
    
    agregado de pantalla para ligar multiplicador de partes

---
+++ HDBackend/Postventa/Modelos/PartesMultiplicador/mdl_Listado_Partes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Postventa.Modelos.PartesMultiplicador
+{
+    public class mdl_Listado_Partes
+    {
+        public string? parte { get; set; }
+    }
+}
---
+++ HDBackend/Postventa/Modelos/PartesMultiplicador/mdl_Partes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Postventa.Modelos.PartesMultiplicador
+{
+    public class mdl_Partes
+    {
+        public int idparte { get; set; }
+        public string? parte { get; set; }
+        public double multiplicador { get; set; }
+        public string? usuario { get; set; }
+
+    }
+}
