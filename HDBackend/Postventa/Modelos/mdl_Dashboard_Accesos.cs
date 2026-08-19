e589f075865d1474f3c43ec228394c6f7cd5800c
gpeol91 <93731144+gpeol91@users.noreply.github.com>
2026-08-19 13:05:09 -0700

    Acutalizacion posventa

--- HDBackend/Postventa/Modelos/mdl_Dashboard_Accesos.cs
+++ HDBackend/Postventa/Modelos/mdl_Dashboard_Accesos.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Postventa.Modelos
+﻿namespace Postventa.Modelos
 {
     public class mdl_Dashboard_Accesos
     {
         public string? dashboard_titulo { get; set; }
         public bool mostrar_dash_refacciones { get; set; }
+        public bool mostrar_dash_servicio { get; set; }
     }
 }
